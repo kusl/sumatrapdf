kusl/sumatrapdf

commit 20d0cee73393e6144b7eb740cd31da55ed72a1cf

display all current notifications and allow canceling progress notifications with the mouse
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@3493 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/Notifications.h
@@ -13,7 +13,7 @@ class MessageWnd;
 
 class MessageWndCallback {
 public:
-    // called right before a MessageWnd is deleted
+    // called after a message has timed out or been canceled
     virtual void CleanUp(MessageWnd *wnd) = 0;
 };
 
@@ -21,32 +21,36 @@ class MessageWnd : public ProgressUpdateUI {
     static const int TIMEOUT_TIMER_ID = 1;
     static const int PROGRESS_WIDTH = 200;
     static const int PROGRESS_HEIGHT = 5;
-    static const int TL_MARGIN = 8;
     static const int PADDING = 6;
 
     HWND self;
     bool hasProgress;
-    bool highlight;
+    bool hasCancel;
+
     HFONT font;
+    bool highlight;
     MessageWndCallback *callback;
+
     // only used for progress notifications
     bool isCanceled;
     int progress;
     int progressWidth;
-    TCHAR *progressMsg;
+    TCHAR *progressMsg; // must contain two %d (for current and total)
 
-    void CreatePopup(HWND parent, const TCHAR *message, int dpi) {
+    void CreatePopup(HWND parent, const TCHAR *message) {
         NONCLIENTMETRICS ncm = { 0 };
         ncm.cbSize = sizeof(ncm);
         SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(ncm), &ncm, 0);
         font = CreateFontIndirect(&ncm.lfMessageFont);
-        progressWidth = MulDiv(PROGRESS_WIDTH, dpi, USER_DEFAULT_SCREEN_DPI);
+
+        HDC hdc = GetDC(parent);
+        progressWidth = MulDiv(PROGRESS_WIDTH, GetDeviceCaps(hdc, LOGPIXELSX), USER_DEFAULT_SCREEN_DPI);
+        ReleaseDC(parent, hdc);
 
         self = CreateWindowEx(WS_EX_TOPMOST, MESSAGE_WND_CLASS_NAME, message, WS_CHILD | SS_CENTER,
                               TL_MARGIN, TL_MARGIN, 0, 0,
                               parent, (HMENU)0, ghinst, NULL);
         SetWindowLongPtr(self, GWLP_USERDATA, (LONG_PTR)this);
-        // TODO: add a Cancel button, if hasProgress
         UpdateWindowPosition(message, true);
         ShowWindow(self, SW_SHOW);
     }
@@ -61,6 +65,10 @@ class MessageWnd : public ProgressUpdateUI {
         ReleaseDC(self, hdc);
 
         RectI rectMsg = RectI::FromRECT(rc);
+        if (hasCancel) {
+            rectMsg.dy = max(rectMsg.dy, 16);
+            rectMsg.dx += 20;
+        }
         rectMsg.Inflate(PADDING, PADDING);
 
         // adjust the window to fit the message (only shrink the window when there's no progress bar)
@@ -77,29 +85,31 @@ class MessageWnd : public ProgressUpdateUI {
     }
 
 public:
-    MessageWnd(WindowInfo *win, const TCHAR *message, int timeoutInMS=0, bool highlight=false, MessageWndCallback *callback=NULL) :
-        hasProgress(false), callback(callback), highlight(highlight), progressMsg(NULL) {
-        CreatePopup(win->hwndCanvas, message, win->dpi);
+    static const int TL_MARGIN = 8;
+
+    MessageWnd(HWND parent, const TCHAR *message, int timeoutInMS=0, bool highlight=false, MessageWndCallback *callback=NULL) :
+        hasProgress(false), hasCancel(!timeoutInMS), callback(callback), highlight(highlight), progressMsg(NULL) {
+        CreatePopup(parent, message);
         if (timeoutInMS)
             SetTimer(self, TIMEOUT_TIMER_ID, timeoutInMS, NULL);
     }
-    MessageWnd(WindowInfo *win, const TCHAR *message, const TCHAR *progressMsg, MessageWndCallback *callback=NULL) :
-        hasProgress(true), callback(callback), highlight(false), isCanceled(false), progress(0) {
+    MessageWnd(HWND parent, const TCHAR *message, const TCHAR *progressMsg, MessageWndCallback *callback=NULL) :
+        hasProgress(true), hasCancel(true), callback(callback), highlight(false), isCanceled(false), progress(0) {
         this->progressMsg = progressMsg ? Str::Dup(progressMsg) : NULL;
-        CreatePopup(win->hwndCanvas, message, win->dpi);
+        CreatePopup(parent, message);
     }
     ~MessageWnd() {
-        if (callback)
-            callback->CleanUp(this);
         DestroyWindow(self);
         DeleteObject(font);
         free(progressMsg);
     }
 
-    virtual bool ProgressUpdate(int count, int total) {
-        progress = limitValue(100 * count / total, 0, 100);
+    HWND hwnd() { return self; }
+
+    virtual bool ProgressUpdate(int current, int total) {
+        progress = limitValue(100 * current / total, 0, 100);
         if (hasProgress && progressMsg) {
-            ScopedMem<TCHAR> message(Str::Format(progressMsg, count, total));
+            ScopedMem<TCHAR> message(Str::Format(progressMsg, current, total));
             MessageUpdate(message);
         }
         return isCanceled;
@@ -109,9 +119,15 @@ public:
         Win::SetText(self, message);
         UpdateWindowPosition(message);
         this->highlight = highlight;
-        InvalidateRect(self, NULL, TRUE);
-        if (timeoutInMS)
+        if (timeoutInMS) {
             SetTimer(self, TIMEOUT_TIMER_ID, timeoutInMS, NULL);
+            hasCancel = false;
+        }
+        InvalidateRect(self, NULL, TRUE);
+    }
+
+    static RectI GetCancelRect(HWND hwnd) {
+        return RectI(ClientRect(hwnd).dx - 16 - PADDING, PADDING, 16, 16);
     }
 
     static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -121,7 +137,10 @@ public:
             return TRUE;
         }
         if (WM_TIMER == message && TIMEOUT_TIMER_ID == wParam) {
-            delete wnd;
+            if (wnd->callback)
+                wnd->callback->CleanUp(wnd);
+            else
+                delete wnd;
             return 0;
         }
         if (WM_PAINT == message && wnd) {
@@ -148,9 +167,13 @@ public:
             RectI rectMsg = rect;
             if (wnd->hasProgress)
                 rectMsg.dy -= PROGRESS_HEIGHT + PADDING / 2;
-
+            if (wnd->hasCancel)
+                rectMsg.dx -= 20;
             ScopedMem<TCHAR> text(Win::GetText(hwnd));
             DrawText(hdc, text, -1, &rectMsg.ToRECT(), DT_SINGLELINE);
+
+            if (wnd->hasCancel)
+                DrawFrameControl(hdc, &GetCancelRect(hwnd).ToRECT(), DFC_CAPTION, DFCS_CAPTIONCLOSE | DFCS_FLAT);
 
             if (wnd->hasProgress) {
                 rect.dx = wnd->progressWidth;
@@ -174,21 +197,68 @@ public:
             EndPaint(hwnd, &ps);
             return WM_PAINT_HANDLED;
         }
+        if (WM_SETCURSOR == message && wnd->hasCancel) {
+            POINT pt;
+            if (GetCursorPos(&pt) && ScreenToClient(hwnd, &pt) &&
+                GetCancelRect(hwnd).Inside(PointI(pt.x, pt.y))) {
+                SetCursor(LoadCursor(NULL, IDC_HAND));
+                return TRUE;
+            }
+        }
+        if (WM_LBUTTONUP == message && wnd->hasCancel) {
+            if (GetCancelRect(hwnd).Inside(PointI(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)))) {
+                if (wnd->callback)
+                    wnd->callback->CleanUp(wnd);
+                else
+                    delete wnd;
+                return 0;
+            }
+        }
         return DefWindowProc(hwnd, message, wParam, lParam);
     }
 };
 
-// TODO: display several windows beneath each other
 class MessageWndList : public MessageWndCallback {
     Vec<MessageWnd *> wnds;
+
+    void MoveBelow(MessageWnd *fix, MessageWnd *move) {
+        RectI rect = WindowRect(fix->hwnd());
+        rect = MapRectToWindow(rect, HWND_DESKTOP, GetParent(fix->hwnd()));
+        SetWindowPos(move->hwnd(), NULL,
+                     rect.x, rect.y + rect.dy + MessageWnd::TL_MARGIN,
+                     0, 0, SWP_NOSIZE | SWP_NOZORDER);
+    }
 
 public:
     ~MessageWndList() { DeleteVecMembers(wnds); }
 
-    void Add(MessageWnd *wnd) { wnds.Append(wnd); }
     bool Contains(MessageWnd *wnd) { return wnds.Find(wnd) != -1; }
-    void Remove(MessageWnd *wnd) { wnds.Remove(wnd); }
-    bool IsEmpty() { return wnds.Count() == 0; }
 
-    virtual void CleanUp(MessageWnd *wnd) { Remove(wnd); }
+    void Add(MessageWnd *wnd) {
+        if (wnds.Count() > 0)
+            MoveBelow(wnds[wnds.Count() - 1], wnd);
+        wnds.Append(wnd);
+    }
+
+    void Remove(MessageWnd *wnd) {
+        int ix = wnds.Find(wnd);
+        if (ix == -1)
+            return;
+        wnds.Remove(wnd);
+        if (ix == 0 && wnds.Count() > 0) {
+            SetWindowPos(wnds[0]->hwnd(), NULL,
+                         MessageWnd::TL_MARGIN, MessageWnd::TL_MARGIN,
+                         0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            ix = 1;
+        }
+        for (; ix < (int)wnds.Count(); ix++)
+            MoveBelow(wnds[ix - 1], wnds[ix]);
+    }
+
+    virtual void CleanUp(MessageWnd *wnd) {
+        if (Contains(wnd)) {
+            Remove(wnd);
+            delete wnd;
+        }
+    }
 };

src/SumatraPDF.cpp
@@ -2959,12 +2959,14 @@ static void CloseWindow(WindowInfo *win, bool quitIfLast, bool forceClose=false)
 
     if (lastWindow && !quitIfLast) {
         /* last window - don't delete it */
+        win->AbortFinding();
         delete win->watcher;
         win->watcher = NULL;
         if (win->tocShow)
             win->HideTocBox();
         win->ClearTocBox();
-        win->AbortFinding();
+        if (win->findCleanup)
+            win->findCleanup->Callback();
         delete win->dm;
         win->dm = NULL;
         free(win->loadedFilePath);
@@ -2981,6 +2983,7 @@ static void CloseWindow(WindowInfo *win, bool quitIfLast, bool forceClose=false)
         DeleteOldSelectionInfo(*win, true);
         win->RedrawAll();
         UpdateFindbox(*win);
+        SetFocus(win->hwndFrame);
     } else {
         HWND hwndToDestroy = win->hwndFrame;
         DeleteWindowInfo(win);
@@ -3086,6 +3089,13 @@ static void PrintToDevice(PrintData& pd, ProgressUpdateUI *progressUI=NULL)
     di.cbSize = sizeof (DOCINFO);
     di.lpszDocName = engine.FileName();
 
+    int current = 0, total = 0;
+    for (size_t i = 0; i < pd.ranges.Count(); i++)
+        total += pd.ranges[i].nToPage - pd.ranges[i].nFromPage + 1;
+    total += pd.sel.Count();
+    if (progressUI)
+        progressUI->ProgressUpdate(current, total);
+
     if (StartDoc(hdc, &di) <= 0)
         return;
 
@@ -3104,11 +3114,6 @@ static void PrintToDevice(PrintData& pd, ProgressUpdateUI *progressUI=NULL)
     bool bPrintPortrait = paperWidth < paperHeight;
     if (pd.orientation)
         bPrintPortrait = DMORIENT_PORTRAIT == pd.orientation;
-
-    int count = 0, total = 0;
-    for (size_t i = 0; i < pd.ranges.Count(); i++)
-        total += pd.ranges[i].nToPage - pd.ranges[i].nFromPage + 1;
-    total += pd.sel.Count();
 
     if (pd.sel.Count() > 0) {
         DBG_OUT(" printing:  drawing bitmap for selection\n");
@@ -3151,8 +3156,8 @@ static void PrintToDevice(PrintData& pd, ProgressUpdateUI *progressUI=NULL)
                 return;
             }
 
-            count++;
-            if (progressUI && !progressUI->ProgressUpdate(count, total)) {
+            current++;
+            if (progressUI && !progressUI->ProgressUpdate(current, total)) {
                 AbortDoc(hdc);
                 return;
             }
@@ -3245,8 +3250,8 @@ static void PrintToDevice(PrintData& pd, ProgressUpdateUI *progressUI=NULL)
                 return;
             }
 
-            count++;
-            if (progressUI && !progressUI->ProgressUpdate(count, total)) {
+            current++;
+            if (progressUI && !progressUI->ProgressUpdate(current, total)) {
                 AbortDoc(hdc);
                 return;
             }
@@ -3258,15 +3263,15 @@ static void PrintToDevice(PrintData& pd, ProgressUpdateUI *progressUI=NULL)
 
 class PrintThreadUpdateWorkItem : public UIThreadWorkItem {
     MessageWnd *wnd;
-    int count, total;
+    int current, total;
 
 public:
-    PrintThreadUpdateWorkItem(WindowInfo *win, MessageWnd *wnd, int count, int total)
-        : UIThreadWorkItem(win), wnd(wnd), count(count), total(total) { }
+    PrintThreadUpdateWorkItem(WindowInfo *win, MessageWnd *wnd, int current, int total)
+        : UIThreadWorkItem(win), wnd(wnd), current(current), total(total) { }
 
     virtual void Execute() {
         if (WindowInfoStillValid(win) && win->messages->Contains(wnd))
-            wnd->ProgressUpdate(count, total);
+            wnd->ProgressUpdate(current, total);
     }
 };
 
@@ -3280,16 +3285,16 @@ public:
 
     PrintThreadWorkItem(WindowInfo *win, PrintData *data) :
         UIThreadWorkItem(win), data(data), isCanceled(false) { 
-        wnd = new MessageWnd(win, _T(""), _TR("Printing page %d of %d..."), this);
+        wnd = new MessageWnd(win->hwndCanvas, _T(""), _TR("Printing page %d of %d..."), this);
         win->messages->Add(wnd);
     }
     ~PrintThreadWorkItem() {
         delete data;
-        delete wnd;
+        CleanUp(wnd);
     }
 
-    virtual bool ProgressUpdate(int count, int total) {
-        gUIThreadMarshaller.Queue(new PrintThreadUpdateWorkItem(win, wnd, count, total));
+    virtual bool ProgressUpdate(int current, int total) {
+        gUIThreadMarshaller.Queue(new PrintThreadUpdateWorkItem(win, wnd, current, total));
         return WindowInfoStillValid(win) && !win->printCanceled && !isCanceled;
     }
 
@@ -3301,7 +3306,8 @@ public:
     virtual void CleanUp(MessageWnd *wnd) {
         isCanceled = true;
         this->wnd = NULL;
-        win->messages->CleanUp(wnd);
+        if (WindowInfoStillValid(win))
+            win->messages->CleanUp(wnd);
     }
 
     virtual void Execute() {
@@ -4723,41 +4729,37 @@ static void ClearSearchResult(WindowInfo& win)
 
 class UpdateFindStatusWorkItem : public UIThreadWorkItem {
     MessageWnd *wnd;
-    int count, total;
+    int current, total;
 
 public:
-    UpdateFindStatusWorkItem(WindowInfo *win, MessageWnd *wnd, int count, int total)
-        : UIThreadWorkItem(win), wnd(wnd), count(count), total(total) { }
+    UpdateFindStatusWorkItem(WindowInfo *win, MessageWnd *wnd, int current, int total)
+        : UIThreadWorkItem(win), wnd(wnd), current(current), total(total) { }
 
     virtual void Execute() {
-        if (WindowInfoStillValid(win) && win->messages->Contains(wnd) && !win->findCanceled)
-            wnd->ProgressUpdate(count, total);
+        if (WindowInfoStillValid(win) && !win->findCanceled && win->messages->Contains(wnd))
+            wnd->ProgressUpdate(current, total);
     }
 };
 
-class FindThreadData : public ProgressUpdateUI, public MessageWndCallback {
+class FindThreadData : public ProgressUpdateUI, public MessageWndCallback, public CallbackFunc, public UIThreadWorkItem {
 public:
-    WindowInfo *win;
     MessageWnd *wnd;
     TextSearchDirection direction;
     bool wasModified;
     TCHAR *text;
 
     FindThreadData(WindowInfo& win, TextSearchDirection direction, HWND findBox) :
-        win(&win), direction(direction), wnd(NULL) {
+        UIThreadWorkItem(&win), wnd(NULL), direction(direction) {
         text = Win::GetText(findBox);
         wasModified = Edit_GetModify(findBox);
     }
-    ~FindThreadData() {
-        delete wnd;
-        free(text);
-    }
+    ~FindThreadData() { free(text); }
 
     void ShowUI() {
         const LPARAM disable = (LPARAM)MAKELONG(0, 0);
 
-        assert(!wnd);
-        wnd = new MessageWnd(win, _T(""), _TR("Searching %d of %d..."), this);
+        wnd = new MessageWnd(win->hwndCanvas, _T(""), _TR("Searching %d of %d..."), this);
+        // let win->messages own the MessageWnd (we'll forward the CleanUp call)
         win->messages->Add(wnd);
 
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_PREV, disable);
@@ -4772,8 +4774,8 @@ public:
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_NEXT, enable);
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_MATCH, enable);
 
-        if (!success && !loopedAround) // i.e. canceled
-            delete wnd;
+        if (!success && !loopedAround || !win->messages->Contains(wnd)) // i.e. canceled
+            CleanUp(wnd);
         else if (!success && loopedAround)
             wnd->MessageUpdate(_TR("No matches were found"), 3000);
         else if (!loopedAround) {
@@ -4785,16 +4787,24 @@ public:
         }    
     }
 
-    virtual bool ProgressUpdate(int count, int total) {
-        if (!win->findCanceled)
-            gUIThreadMarshaller.Queue(new UpdateFindStatusWorkItem(win, wnd, count, total));
-        return !win->findCanceled;
+    virtual bool ProgressUpdate(int current, int total) {
+        gUIThreadMarshaller.Queue(new UpdateFindStatusWorkItem(win, wnd, current, total));
+        return WindowInfoStillValid(win) && !win->findCanceled && win->messages->Contains(wnd);
     }
 
     virtual void CleanUp(MessageWnd *wnd) {
+        if (!WindowInfoStillValid(win) || !this->wnd)
+            return;
+        win->findCleanup = NULL;
         this->wnd = NULL;
         win->messages->CleanUp(wnd);
+
+        gUIThreadMarshaller.Queue(this);
     }
+
+    virtual void Callback() { CleanUp(wnd); }
+
+    virtual void Execute() { }
 };
 
 class FindEndWorkItem : public UIThreadWorkItem {
@@ -4812,14 +4822,11 @@ public:
         UIThreadWorkItem(win), ftd(ftd), textSel(textSel),
             loopedAround(loopedAround),
             wasModifiedCanceled(wasModifiedCanceled) { }
-    ~FindEndWorkItem() { delete ftd; }
 
     virtual void Execute() {
-        if (!WindowInfoStillValid(win))
+        if (!WindowInfoStillValid(win) || !win->IsDocLoaded())
             return;
-        if (!win->IsDocLoaded()) {
-            // let FindThreadData's destructor hide the progress window
-        } else if (textSel) {
+        if (textSel) {
             ShowSearchResult(*win, textSel, wasModifiedCanceled);
             ftd->HideUI(true, loopedAround);
         } else {
@@ -4878,6 +4885,10 @@ static void FindTextOnThread(WindowInfo& win, TextSearchDirection direction)
         delete ftd;
         return;
     }
+
+    if (win.findCleanup)
+        win.findCleanup->Callback();
+    win.findCleanup = ftd;
     ftd->ShowUI();
     win.findThread = CreateThread(NULL, 0, FindThread, ftd, 0, 0);
 }

src/WindowInfo.cpp
@@ -21,7 +21,7 @@ WindowInfo::WindowInfo(HWND hwnd) :
     hwndTocBox(NULL), hwndTocTree(NULL), hwndSpliter(NULL),
     hwndInfotip(NULL), infotipVisible(false), hwndProperties(NULL),
     findThread(NULL), findCanceled(false), printThread(NULL), printCanceled(false),
-    showSelection(false), mouseAction(MA_IDLE),
+    findCleanup(NULL), showSelection(false), mouseAction(MA_IDLE),
     prevZoomVirtual(INVALID_ZOOM), prevDisplayMode(DM_AUTOMATIC),
     loadedFilePath(NULL), currPageNo(0),
     xScrollSpeed(0), yScrollSpeed(0), wheelAccumDelta(0),
@@ -54,7 +54,9 @@ WindowInfo::~WindowInfo() {
     delete this->buffer;
     delete this->selectionOnPage;
     delete this->linkOnLastButtonDown;
+
     delete this->messages;
+    delete this->findCleanup;
 
     free(this->loadedFilePath);
 
@@ -95,12 +97,9 @@ SizeI WindowInfo::GetViewPortSize()
     return size;
 }
 
-void WindowInfo::ShowNotification(const TCHAR *message, bool highlight)
+void WindowInfo::ShowNotification(const TCHAR *message, bool autoDismiss, bool highlight)
 {
-    // TODO: ensure that all currently displayed notification are visible
-    //       (e.g. stacked beneath each other)
-    if (messages->IsEmpty())
-        messages->Add(new MessageWnd(this, message, 3000, highlight, messages));
+    messages->Add(new MessageWnd(this->hwndCanvas, message, autoDismiss ? 3000 : 0, highlight, messages));
 }
 
 void WindowInfo::AbortFinding()

commit 2cf107c839f7207103edc5dd3c448b89053d4cfb

fix crash caused by canceling a search (introduced with r3493)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@3494 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/Notifications.h
@@ -262,3 +262,28 @@ public:
         }
     }
 };
+
+class MessageWndHolder : public MessageWndCallback {
+    MessageWnd *wnd;
+    MessageWndList *list;
+
+public:
+    MessageWndHolder(MessageWndList *list) : list(list), wnd(NULL) { }
+    ~MessageWndHolder() {
+        if (wnd)
+            list->CleanUp(wnd);
+    }
+
+    void SetUp(MessageWnd *wnd) {
+        assert(!this->wnd);
+        this->wnd = wnd;
+        list->Add(wnd);
+    }
+
+    virtual void CleanUp(MessageWnd *wnd) {
+        this->wnd = NULL;
+        list->CleanUp(wnd);
+    }
+
+    MessageWnd *GetWnd() const { return wnd; }
+};

src/SumatraPDF.cpp
@@ -2959,20 +2959,19 @@ static void CloseWindow(WindowInfo *win, bool quitIfLast, bool forceClose=false)
 
     if (lastWindow && !quitIfLast) {
         /* last window - don't delete it */
-        win->AbortFinding();
         delete win->watcher;
         win->watcher = NULL;
         if (win->tocShow)
             win->HideTocBox();
         win->ClearTocBox();
-        if (win->findCleanup)
-            win->findCleanup->Callback();
+        win->AbortFinding(true);
         delete win->dm;
         win->dm = NULL;
         free(win->loadedFilePath);
         win->loadedFilePath = NULL;
         delete win->pdfsync;
         win->pdfsync = NULL;
+        // TODO: hide all document specific messages (also cancel printing?)
 
         if (win->hwndProperties) {
             DestroyWindow(win->hwndProperties);
@@ -4741,41 +4740,42 @@ public:
     }
 };
 
-class FindThreadData : public ProgressUpdateUI, public MessageWndCallback, public CallbackFunc, public UIThreadWorkItem {
-public:
-    MessageWnd *wnd;
+struct FindThreadData : public ProgressUpdateUI {
+    WindowInfo *win;
     TextSearchDirection direction;
     bool wasModified;
     TCHAR *text;
 
     FindThreadData(WindowInfo& win, TextSearchDirection direction, HWND findBox) :
-        UIThreadWorkItem(&win), wnd(NULL), direction(direction) {
+        win(&win), direction(direction) {
         text = Win::GetText(findBox);
         wasModified = Edit_GetModify(findBox);
     }
     ~FindThreadData() { free(text); }
 
-    void ShowUI() {
+    void ShowUI() const {
         const LPARAM disable = (LPARAM)MAKELONG(0, 0);
 
-        wnd = new MessageWnd(win->hwndCanvas, _T(""), _TR("Searching %d of %d..."), this);
-        // let win->messages own the MessageWnd (we'll forward the CleanUp call)
-        win->messages->Add(wnd);
+        assert(!win->findHelper);
+        win->findHelper = new MessageWndHolder(win->messages);
+        MessageWnd *wnd = new MessageWnd(win->hwndCanvas, _T(""), _TR("Searching %d of %d..."), win->findHelper);
+        // let win->findHelper own the MessageWnd (FindThreadData might get deleted before)
+        win->findHelper->SetUp(wnd);
 
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_PREV, disable);
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_NEXT, disable);
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_MATCH, disable);
     }
 
-    void HideUI(bool success, bool loopedAround) {
+    void HideUI(MessageWnd *wnd, bool success, bool loopedAround) const {
         LPARAM enable = (LPARAM)MAKELONG(1, 0);
 
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_PREV, enable);
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_NEXT, enable);
         SendMessage(win->hwndToolbar, TB_ENABLEBUTTON, IDM_FIND_MATCH, enable);
 
-        if (!success && !loopedAround || !win->messages->Contains(wnd)) // i.e. canceled
-            CleanUp(wnd);
+        if (!success && !loopedAround || !wnd) // i.e. canceled
+            win->findHelper->CleanUp(wnd);
         else if (!success && loopedAround)
             wnd->MessageUpdate(_TR("No matches were found"), 3000);
         else if (!loopedAround) {
@@ -4788,23 +4788,11 @@ public:
     }
 
     virtual bool ProgressUpdate(int current, int total) {
-        gUIThreadMarshaller.Queue(new UpdateFindStatusWorkItem(win, wnd, current, total));
-        return WindowInfoStillValid(win) && !win->findCanceled && win->messages->Contains(wnd);
+        if (!WindowInfoStillValid(win) || !win->findHelper || !win->findHelper->GetWnd() || win->findCanceled)
+            return false;
+        gUIThreadMarshaller.Queue(new UpdateFindStatusWorkItem(win, win->findHelper->GetWnd(), current, total));
+        return true;
     }
-
-    virtual void CleanUp(MessageWnd *wnd) {
-        if (!WindowInfoStillValid(win) || !this->wnd)
-            return;
-        win->findCleanup = NULL;
-        this->wnd = NULL;
-        win->messages->CleanUp(wnd);
-
-        gUIThreadMarshaller.Queue(this);
-    }
-
-    virtual void Callback() { CleanUp(wnd); }
-
-    virtual void Execute() { }
 };
 
 class FindEndWorkItem : public UIThreadWorkItem {
@@ -4814,25 +4802,24 @@ class FindEndWorkItem : public UIThreadWorkItem {
     bool    loopedAround;
 
 public:
-    FindEndWorkItem(WindowInfo *win,
-                    FindThreadData *ftd,
-                    TextSel *textSel,
-                    bool wasModifiedCanceled,
-                    bool loopedAround=false) :
+    FindEndWorkItem(WindowInfo *win, FindThreadData *ftd, TextSel *textSel,
+                    bool wasModifiedCanceled, bool loopedAround=false) :
         UIThreadWorkItem(win), ftd(ftd), textSel(textSel),
-            loopedAround(loopedAround),
-            wasModifiedCanceled(wasModifiedCanceled) { }
+        loopedAround(loopedAround), wasModifiedCanceled(wasModifiedCanceled) { }
+    ~FindEndWorkItem() { delete ftd; }
 
     virtual void Execute() {
-        if (!WindowInfoStillValid(win) || !win->IsDocLoaded())
+        if (!WindowInfoStillValid(win))
             return;
-        if (textSel) {
+        if (!win->IsDocLoaded() || !win->findHelper) {
+            // the UI has already been disabled and hidden
+        } else if (textSel) {
             ShowSearchResult(*win, textSel, wasModifiedCanceled);
-            ftd->HideUI(true, loopedAround);
+            ftd->HideUI(win->findHelper->GetWnd(), true, loopedAround);
         } else {
             // nothing found or search canceled
             ClearSearchResult(*win);
-            ftd->HideUI(false, !wasModifiedCanceled);
+            ftd->HideUI(win->findHelper->GetWnd(), false, !wasModifiedCanceled);
         }
 
         HANDLE hThread = win->findThread;
@@ -4876,7 +4863,7 @@ static DWORD WINAPI FindThread(LPVOID data)
 
 static void FindTextOnThread(WindowInfo& win, TextSearchDirection direction)
 {
-    win.AbortFinding();
+    win.AbortFinding(true);
 
     FindThreadData *ftd = new FindThreadData(win, direction, win.hwndFindBox);
     Edit_SetModify(win.hwndFindBox, FALSE);
@@ -4886,9 +4873,6 @@ static void FindTextOnThread(WindowInfo& win, TextSearchDirection direction)
         return;
     }
 
-    if (win.findCleanup)
-        win.findCleanup->Callback();
-    win.findCleanup = ftd;
     ftd->ShowUI();
     win.findThread = CreateThread(NULL, 0, FindThread, ftd, 0, 0);
 }

src/WindowInfo.cpp
@@ -21,7 +21,7 @@ WindowInfo::WindowInfo(HWND hwnd) :
     hwndTocBox(NULL), hwndTocTree(NULL), hwndSpliter(NULL),
     hwndInfotip(NULL), infotipVisible(false), hwndProperties(NULL),
     findThread(NULL), findCanceled(false), printThread(NULL), printCanceled(false),
-    findCleanup(NULL), showSelection(false), mouseAction(MA_IDLE),
+    findHelper(NULL), showSelection(false), mouseAction(MA_IDLE),
     prevZoomVirtual(INVALID_ZOOM), prevDisplayMode(DM_AUTOMATIC),
     loadedFilePath(NULL), currPageNo(0),
     xScrollSpeed(0), yScrollSpeed(0), wheelAccumDelta(0),
@@ -55,8 +55,8 @@ WindowInfo::~WindowInfo() {
     delete this->selectionOnPage;
     delete this->linkOnLastButtonDown;
 
+    delete this->findHelper;
     delete this->messages;
-    delete this->findCleanup;
 
     free(this->loadedFilePath);
 
@@ -102,13 +102,18 @@ void WindowInfo::ShowNotification(const TCHAR *message, bool autoDismiss, bool h
     messages->Add(new MessageWnd(this->hwndCanvas, message, autoDismiss ? 3000 : 0, highlight, messages));
 }
 
-void WindowInfo::AbortFinding()
+void WindowInfo::AbortFinding(bool hideMessage)
 {
     if (this->findThread) {
         this->findCanceled = true;
         WaitForSingleObject(this->findThread, INFINITE);
     }
     this->findCanceled = false;
+
+    if (hideMessage && this->findHelper) {
+        delete this->findHelper;
+        this->findHelper = NULL;
+    }
 }
 
 void WindowInfo::AbortPrinting()

src/WindowInfo.h
@@ -14,6 +14,7 @@ class DoubleBuffer;
 class SelectionOnPage;
 class LinkHandler;
 class MessageWndList;
+class MessageWndHolder;
 
 /* Describes actions which can be performed by mouse */
 enum MouseAction {
@@ -136,7 +137,7 @@ public:
     bool            threadStressRunning;
 
     MessageWndList *messages;
-    CallbackFunc *  findCleanup; // set while there's a find notification to delete
+    MessageWndHolder *findHelper;
 
     HANDLE          printThread;
     bool            printCanceled;
@@ -187,7 +188,7 @@ public:
     void SwitchToDisplayMode(DisplayMode displayMode, bool keepContinuous=false);
     void MoveDocBy(int dx, int dy);
     void AbortPrinting();
-    void AbortFinding();
+    void AbortFinding(bool hideMessage=false);
 
     void ShowTocBox();
     void HideTocBox();