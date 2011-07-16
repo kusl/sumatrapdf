kusl/sumatrapdf

commit 97b5b6fb3eb50b26000a47cecae81eb55e230553

merge latest MuPDF update
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4069 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/fitz/dev_list.c
@@ -2,6 +2,8 @@
 
 typedef struct fz_display_node_s fz_display_node;
 
+#define STACK_SIZE 96
+
 typedef enum fz_display_command_e
 {
 	FZ_CMD_FILL_PATH,
@@ -50,6 +52,13 @@ struct fz_display_list_s
 {
 	fz_display_node *first;
 	fz_display_node *last;
+
+	int top;
+	struct {
+		fz_rect *update;
+		fz_rect rect;
+	} stack[STACK_SIZE];
+	int tiled;
 };
 
 enum { ISOLATED = 1, KNOCKOUT = 2 };
@@ -98,6 +107,76 @@ fz_clone_stroke_state(fz_stroke_state *stroke)
 static void
 fz_append_display_node(fz_display_list *list, fz_display_node *node)
 {
+	switch (node->cmd) {
+	case FZ_CMD_CLIP_PATH:
+	case FZ_CMD_CLIP_STROKE_PATH:
+	case FZ_CMD_CLIP_IMAGE_MASK:
+		/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692336 */
+		if (list->top >= STACK_SIZE)
+		{
+			list->top++;
+			break;
+		}
+		list->stack[list->top].update = &node->rect;
+		list->stack[list->top].rect = fz_empty_rect;
+		list->top++;
+		break;
+	case FZ_CMD_END_MASK: /* SumatraPDF: masks clip as well */
+	case FZ_CMD_CLIP_TEXT:
+	case FZ_CMD_CLIP_STROKE_TEXT:
+		/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692336 */
+		if (list->top >= STACK_SIZE)
+		{
+			list->top++;
+			break;
+		}
+		list->stack[list->top].update = NULL;
+		list->stack[list->top].rect = fz_empty_rect;
+		list->top++;
+		break;
+	case FZ_CMD_BEGIN_TILE:
+		list->tiled++;
+		/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692336 */
+		if (list->top > 0 && list->top <= STACK_SIZE) {
+			list->stack[list->top-1].rect = fz_infinite_rect;
+		}
+		break;
+	case FZ_CMD_END_TILE:
+		list->tiled--;
+		break;
+	case FZ_CMD_END_GROUP:
+		break;
+	case FZ_CMD_POP_CLIP:
+		/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692336 */
+		if (list->top > STACK_SIZE)
+		{
+			list->top--;
+			node->rect = fz_infinite_rect;
+		}
+		else
+		if (list->top > 0) {
+			fz_rect *update;
+			list->top--;
+			update = list->stack[list->top].update;
+			if (list->tiled == 0) {
+				if (update != NULL) {
+					*update = fz_intersect_rect(*update, list->stack[list->top].rect);
+					node->rect = *update;
+				} else {
+					node->rect = list->stack[list->top].rect;
+				}
+			} else {
+				node->rect = fz_infinite_rect;
+			}
+		}
+		/*@fallthrough@*/
+	default:
+		/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692336 */
+		if ((list->top > 0) && (list->tiled == 0) && list->top <= STACK_SIZE) {
+			list->stack[list->top-1].rect = fz_union_rect(list->stack[list->top-1].rect, node->rect);
+		}
+		break;
+	}
 	if (!list->first)
 	{
 		list->first = node;
@@ -177,22 +256,26 @@ fz_list_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_matri
 }
 
 static void
-fz_list_clip_path(void *user, fz_path *path, int even_odd, fz_matrix ctm)
+fz_list_clip_path(void *user, fz_path *path, fz_rect *rect, int even_odd, fz_matrix ctm)
 {
 	fz_display_node *node;
 	node = fz_new_display_node(FZ_CMD_CLIP_PATH, ctm, NULL, NULL, 0);
 	node->rect = fz_bound_path(path, NULL, ctm);
+	if (rect != NULL)
+		node->rect = fz_intersect_rect(node->rect, *rect);
 	node->item.path = fz_clone_path(path);
 	node->flag = even_odd;
 	fz_append_display_node(user, node);
 }
 
 static void
-fz_list_clip_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_matrix ctm)
+fz_list_clip_stroke_path(void *user, fz_path *path, fz_rect *rect, fz_stroke_state *stroke, fz_matrix ctm)
 {
 	fz_display_node *node;
 	node = fz_new_display_node(FZ_CMD_CLIP_STROKE_PATH, ctm, NULL, NULL, 0);
 	node->rect = fz_bound_path(path, stroke, ctm);
+	if (rect != NULL)
+		node->rect = fz_intersect_rect(node->rect, *rect);
 	node->item.path = fz_clone_path(path);
 	node->stroke = fz_clone_stroke_state(stroke);
 	fz_append_display_node(user, node);
@@ -261,7 +344,6 @@ fz_list_pop_clip(void *user)
 {
 	fz_display_node *node;
 	node = fz_new_display_node(FZ_CMD_POP_CLIP, fz_identity, NULL, NULL, 0);
-	/* TODO: scan back for matching pushclip and calculate bbox of contents */
 	fz_append_display_node(user, node);
 }
 
@@ -297,11 +379,13 @@ fz_list_fill_image_mask(void *user, fz_pixmap *image, fz_matrix ctm,
 }
 
 static void
-fz_list_clip_image_mask(void *user, fz_pixmap *image, fz_matrix ctm)
+fz_list_clip_image_mask(void *user, fz_pixmap *image, fz_rect *rect, fz_matrix ctm)
 {
 	fz_display_node *node;
 	node = fz_new_display_node(FZ_CMD_CLIP_IMAGE_MASK, ctm, NULL, NULL, 0);
 	node->rect = fz_transform_rect(ctm, fz_unit_rect);
+	if (rect != NULL)
+		node->rect = fz_intersect_rect(node->rect, *rect);
 	node->item.image = fz_keep_pixmap(image);
 	fz_append_display_node(user, node);
 }
@@ -407,6 +491,8 @@ fz_new_display_list(void)
 	fz_display_list *list = fz_malloc(sizeof(fz_display_list));
 	list->first = NULL;
 	list->last = NULL;
+	list->top = 0;
+	list->tiled = 0;
 	return list;
 }
 
@@ -503,11 +589,17 @@ visible:
 				node->colorspace, node->color, node->alpha);
 			break;
 		case FZ_CMD_CLIP_PATH:
-			fz_clip_path(dev, node->item.path, node->flag, ctm);
+		{
+			fz_rect trect = fz_transform_rect(top_ctm, node->rect);
+			fz_clip_path(dev, node->item.path, &trect, node->flag, ctm);
 			break;
+		}
 		case FZ_CMD_CLIP_STROKE_PATH:
-			fz_clip_stroke_path(dev, node->item.path, node->stroke, ctm);
+		{
+			fz_rect trect = fz_transform_rect(top_ctm, node->rect);
+			fz_clip_stroke_path(dev, node->item.path, &trect, node->stroke, ctm);
 			break;
+		}
 		case FZ_CMD_FILL_TEXT:
 			fz_fill_text(dev, node->item.text, ctm,
 				node->colorspace, node->color, node->alpha);
@@ -536,8 +628,11 @@ visible:
 				node->colorspace, node->color, node->alpha);
 			break;
 		case FZ_CMD_CLIP_IMAGE_MASK:
-			fz_clip_image_mask(dev, node->item.image, ctm);
+		{
+			fz_rect trect = fz_transform_rect(top_ctm, node->rect);
+			fz_clip_image_mask(dev, node->item.image, &trect, ctm);
 			break;
+		}
 		case FZ_CMD_POP_CLIP:
 			fz_pop_clip(dev);
 			break;

mupdf/draw/draw_device.c
@@ -100,7 +100,7 @@ fz_draw_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_matri
 }
 
 static void
-fz_draw_clip_path(void *user, fz_path *path, int even_odd, fz_matrix ctm)
+fz_draw_clip_path(void *user, fz_path *path, fz_rect *rect, int even_odd, fz_matrix ctm)
 {
 	fz_draw_device *dev = user;
 	fz_colorspace *model = dev->dest->colorspace;
@@ -121,6 +121,9 @@ fz_draw_clip_path(void *user, fz_path *path, int even_odd, fz_matrix ctm)
 
 	bbox = fz_bound_gel(dev->gel);
 	bbox = fz_intersect_bbox(bbox, dev->scissor);
+	if (rect) {
+		bbox = fz_intersect_bbox(bbox, fz_round_rect(*rect));
+	}
 
 	if (fz_is_empty_rect(bbox) || fz_is_rect_gel(dev->gel))
 	{
@@ -149,7 +152,7 @@ fz_draw_clip_path(void *user, fz_path *path, int even_odd, fz_matrix ctm)
 }
 
 static void
-fz_draw_clip_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_matrix ctm)
+fz_draw_clip_stroke_path(void *user, fz_path *path, fz_rect *rect, fz_stroke_state *stroke, fz_matrix ctm)
 {
 	fz_draw_device *dev = user;
 	fz_colorspace *model = dev->dest->colorspace;
@@ -177,6 +180,8 @@ fz_draw_clip_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_
 
 	bbox = fz_bound_gel(dev->gel);
 	bbox = fz_intersect_bbox(bbox, dev->scissor);
+	if (rect)
+		bbox = fz_intersect_bbox(bbox, fz_round_rect(*rect));
 
 	mask = fz_new_pixmap_with_rect(NULL, bbox);
 	dest = fz_new_pixmap_with_rect(model, bbox);
@@ -681,7 +686,7 @@ fz_draw_fill_image_mask(void *user, fz_pixmap *image, fz_matrix ctm,
 }
 
 static void
-fz_draw_clip_image_mask(void *user, fz_pixmap *image, fz_matrix ctm)
+fz_draw_clip_image_mask(void *user, fz_pixmap *image, fz_rect *rect, fz_matrix ctm)
 {
 	fz_draw_device *dev = user;
 	fz_colorspace *model = dev->dest->colorspace;
@@ -708,6 +713,8 @@ fz_draw_clip_image_mask(void *user, fz_pixmap *image, fz_matrix ctm)
 
 	bbox = fz_round_rect(fz_transform_rect(ctm, fz_unit_rect));
 	bbox = fz_intersect_bbox(bbox, dev->scissor);
+	if (rect)
+		bbox = fz_intersect_bbox(bbox, fz_round_rect(*rect));
 
 	mask = fz_new_pixmap_with_rect(NULL, bbox);
 	dest = fz_new_pixmap_with_rect(model, bbox);

mupdf/fitz/base_geometry.c
@@ -166,6 +166,36 @@ fz_round_rect(fz_rect f)
 	return i;
 }
 
+fz_rect
+fz_intersect_rect(fz_rect a, fz_rect b)
+{
+	fz_rect r;
+	if (fz_is_infinite_rect(a)) return b;
+	if (fz_is_infinite_rect(b)) return a;
+	if (fz_is_empty_rect(a)) return fz_empty_rect;
+	if (fz_is_empty_rect(b)) return fz_empty_rect;
+	r.x0 = MAX(a.x0, b.x0);
+	r.y0 = MAX(a.y0, b.y0);
+	r.x1 = MIN(a.x1, b.x1);
+	r.y1 = MIN(a.y1, b.y1);
+	return (r.x1 < r.x0 || r.y1 < r.y0) ? fz_empty_rect : r;
+}
+
+fz_rect
+fz_union_rect(fz_rect a, fz_rect b)
+{
+	fz_rect r;
+	if (fz_is_infinite_rect(a)) return a;
+	if (fz_is_infinite_rect(b)) return b;
+	if (fz_is_empty_rect(a)) return b;
+	if (fz_is_empty_rect(b)) return a;
+	r.x0 = MIN(a.x0, b.x0);
+	r.y0 = MIN(a.y0, b.y0);
+	r.x1 = MAX(a.x1, b.x1);
+	r.y1 = MAX(a.y1, b.y1);
+	return r;
+}
+
 fz_bbox
 fz_intersect_bbox(fz_bbox a, fz_bbox b)
 {

mupdf/fitz/dev_gdiplus.cpp
@@ -843,7 +843,7 @@ fz_gdiplus_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_ma
 }
 
 extern "C" static void
-fz_gdiplus_clip_path(void *user, fz_path *path, int evenodd, fz_matrix ctm)
+fz_gdiplus_clip_path(void *user, fz_path *path, fz_rect *rect, int evenodd, fz_matrix ctm)
 {
 	GraphicsPath *gpath = gdiplus_get_path(path, ctm, evenodd);
 	
@@ -857,7 +857,7 @@ fz_gdiplus_clip_path(void *user, fz_path *path, int evenodd, fz_matrix ctm)
 }
 
 extern "C" static void
-fz_gdiplus_clip_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_matrix ctm)
+fz_gdiplus_clip_stroke_path(void *user, fz_path *path, fz_rect *rect, fz_stroke_state *stroke, fz_matrix ctm)
 {
 	GraphicsPath *gpath = gdiplus_get_path(path, ctm);
 	
@@ -1243,7 +1243,7 @@ fz_gdiplus_fill_image_mask(void *user, fz_pixmap *image, fz_matrix ctm,
 }
 
 extern "C" static void
-fz_gdiplus_clip_image_mask(void *user, fz_pixmap *image, fz_matrix ctm)
+fz_gdiplus_clip_image_mask(void *user, fz_pixmap *image, fz_rect *rect, fz_matrix ctm)
 {
 	((userData *)user)->pushClipMask(image, ctm);
 }

mupdf/fitz/dev_null.c
@@ -35,17 +35,17 @@ fz_stroke_path(fz_device *dev, fz_path *path, fz_stroke_state *stroke, fz_matrix
 }
 
 void
-fz_clip_path(fz_device *dev, fz_path *path, int even_odd, fz_matrix ctm)
+fz_clip_path(fz_device *dev, fz_path *path, fz_rect *rect, int even_odd, fz_matrix ctm)
 {
 	if (dev->clip_path)
-		dev->clip_path(dev->user, path, even_odd, ctm);
+		dev->clip_path(dev->user, path, rect, even_odd, ctm);
 }
 
 void
-fz_clip_stroke_path(fz_device *dev, fz_path *path, fz_stroke_state *stroke, fz_matrix ctm)
+fz_clip_stroke_path(fz_device *dev, fz_path *path, fz_rect *rect, fz_stroke_state *stroke, fz_matrix ctm)
 {
 	if (dev->clip_stroke_path)
-		dev->clip_stroke_path(dev->user, path, stroke, ctm);
+		dev->clip_stroke_path(dev->user, path, rect, stroke, ctm);
 }
 
 void
@@ -115,10 +115,10 @@ fz_fill_image_mask(fz_device *dev, fz_pixmap *image, fz_matrix ctm,
 }
 
 void
-fz_clip_image_mask(fz_device *dev, fz_pixmap *image, fz_matrix ctm)
+fz_clip_image_mask(fz_device *dev, fz_pixmap *image, fz_rect *rect, fz_matrix ctm)
 {
 	if (dev->clip_image_mask)
-		dev->clip_image_mask(dev->user, image, ctm);
+		dev->clip_image_mask(dev->user, image, rect, ctm);
 }
 
 void

mupdf/fitz/dev_trace.c
@@ -105,7 +105,7 @@ fz_trace_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_matr
 }
 
 static void
-fz_trace_clip_path(void *user, fz_path *path, int even_odd, fz_matrix ctm)
+fz_trace_clip_path(void *user, fz_path *path, fz_rect *rect, int even_odd, fz_matrix ctm)
 {
 	printf("<clip_path ");
 	if (even_odd)
@@ -119,7 +119,7 @@ fz_trace_clip_path(void *user, fz_path *path, int even_odd, fz_matrix ctm)
 }
 
 static void
-fz_trace_clip_stroke_path(void *user, fz_path *path, fz_stroke_state *stroke, fz_matrix ctm)
+fz_trace_clip_stroke_path(void *user, fz_path *path, fz_rect *rect, fz_stroke_state *stroke, fz_matrix ctm)
 {
 	printf("<clip_stroke_path ");
 	fz_trace_matrix(ctm);
@@ -210,7 +210,7 @@ fz_colorspace *colorspace, float *color, float alpha)
 }
 
 static void
-fz_trace_clip_image_mask(void *user, fz_pixmap *image, fz_matrix ctm)
+fz_trace_clip_image_mask(void *user, fz_pixmap *image, fz_rect *rect, fz_matrix ctm)
 {
 	printf("<clip_image_mask ");
 	fz_trace_matrix(ctm);

mupdf/fitz/fitz.h
@@ -273,7 +273,9 @@ float fz_matrix_expansion(fz_matrix m);
 
 fz_bbox fz_round_rect(fz_rect r);
 fz_bbox fz_intersect_bbox(fz_bbox a, fz_bbox b);
+fz_rect fz_intersect_rect(fz_rect a, fz_rect b);
 fz_bbox fz_union_bbox(fz_bbox a, fz_bbox b);
+fz_rect fz_union_rect(fz_rect a, fz_rect b);
 
 fz_point fz_transform_point(fz_matrix m, fz_point p);
 fz_point fz_transform_vector(fz_matrix m, fz_point p);
@@ -963,8 +965,8 @@ struct fz_device_s
 
 	void (*fill_path)(void *, fz_path *, int even_odd, fz_matrix, fz_colorspace *, float *color, float alpha);
 	void (*stroke_path)(void *, fz_path *, fz_stroke_state *, fz_matrix, fz_colorspace *, float *color, float alpha);
-	void (*clip_path)(void *, fz_path *, int even_odd, fz_matrix);
-	void (*clip_stroke_path)(void *, fz_path *, fz_stroke_state *, fz_matrix);
+	void (*clip_path)(void *, fz_path *, fz_rect *rect, int even_odd, fz_matrix);
+	void (*clip_stroke_path)(void *, fz_path *, fz_rect *rect, fz_stroke_state *, fz_matrix);
 
 	void (*fill_text)(void *, fz_text *, fz_matrix, fz_colorspace *, float *color, float alpha);
 	void (*stroke_text)(void *, fz_text *, fz_stroke_state *, fz_matrix, fz_colorspace *, float *color, float alpha);
@@ -975,7 +977,7 @@ struct fz_device_s
 	void (*fill_shade)(void *, fz_shade *shd, fz_matrix ctm, float alpha);
 	void (*fill_image)(void *, fz_pixmap *img, fz_matrix ctm, float alpha);
 	void (*fill_image_mask)(void *, fz_pixmap *img, fz_matrix ctm, fz_colorspace *, float *color, float alpha);
-	void (*clip_image_mask)(void *, fz_pixmap *img, fz_matrix ctm);
+	void (*clip_image_mask)(void *, fz_pixmap *img, fz_rect *rect, fz_matrix ctm);
 
 	void (*pop_clip)(void *);
 
@@ -990,8 +992,8 @@ struct fz_device_s
 
 void fz_fill_path(fz_device *dev, fz_path *path, int even_odd, fz_matrix ctm, fz_colorspace *colorspace, float *color, float alpha);
 void fz_stroke_path(fz_device *dev, fz_path *path, fz_stroke_state *stroke, fz_matrix ctm, fz_colorspace *colorspace, float *color, float alpha);
-void fz_clip_path(fz_device *dev, fz_path *path, int even_odd, fz_matrix ctm);
-void fz_clip_stroke_path(fz_device *dev, fz_path *path, fz_stroke_state *stroke, fz_matrix ctm);
+void fz_clip_path(fz_device *dev, fz_path *path, fz_rect *rect, int even_odd, fz_matrix ctm);
+void fz_clip_stroke_path(fz_device *dev, fz_path *path, fz_rect *rect, fz_stroke_state *stroke, fz_matrix ctm);
 void fz_fill_text(fz_device *dev, fz_text *text, fz_matrix ctm, fz_colorspace *colorspace, float *color, float alpha);
 void fz_stroke_text(fz_device *dev, fz_text *text, fz_stroke_state *stroke, fz_matrix ctm, fz_colorspace *colorspace, float *color, float alpha);
 void fz_clip_text(fz_device *dev, fz_text *text, fz_matrix ctm, int accumulate);
@@ -1001,7 +1003,7 @@ void fz_pop_clip(fz_device *dev);
 void fz_fill_shade(fz_device *dev, fz_shade *shade, fz_matrix ctm, float alpha);
 void fz_fill_image(fz_device *dev, fz_pixmap *image, fz_matrix ctm, float alpha);
 void fz_fill_image_mask(fz_device *dev, fz_pixmap *image, fz_matrix ctm, fz_colorspace *colorspace, float *color, float alpha);
-void fz_clip_image_mask(fz_device *dev, fz_pixmap *image, fz_matrix ctm);
+void fz_clip_image_mask(fz_device *dev, fz_pixmap *image, fz_rect *rect, fz_matrix ctm);
 void fz_begin_mask(fz_device *dev, fz_rect area, int luminosity, fz_colorspace *colorspace, float *bc);
 void fz_end_mask(fz_device *dev);
 void fz_begin_group(fz_device *dev, fz_rect area, int isolated, int knockout, int blendmode, float alpha);

mupdf/fitz/res_path.c
@@ -162,7 +162,7 @@ fz_bound_path(fz_path *path, fz_stroke_state *stroke, fz_matrix ctm)
 
 	if (stroke)
 	{
-		float miterlength = sinf(stroke->miterlimit * 0.5f);
+		float miterlength = stroke->miterlimit;
 		float linewidth = stroke->linewidth;
 		float expand = MAX(miterlength, linewidth) * 0.5f;
 		r.x0 -= expand;

commit 0f52fba47c02e8e2070a0f91cdb48ebb272ddcd0

fix rendering regression introduced with r4069
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4184 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/fitz/dev_list.c
@@ -159,6 +159,16 @@ fz_append_display_node(fz_display_list *list, fz_display_node *node)
 			list->top--;
 			update = list->stack[list->top].update;
 			if (list->tiled == 0) {
+				/* cf. http://bugs.ghostscript.com/show_bug.cgi?id=692346 */
+				if (!fz_is_infinite_rect(list->stack[list->top].rect))
+				{
+					/* add some fuzz at the edges, as especially glyph rects
+					 * are sometimes not actually completely bounding the glyph */
+					list->stack[list->top].rect.x0 -= 20;
+					list->stack[list->top].rect.y0 -= 20;
+					list->stack[list->top].rect.x1 += 20;
+					list->stack[list->top].rect.y1 += 20;
+				}
 				if (update != NULL) {
 					*update = fz_intersect_rect(*update, list->stack[list->top].rect);
 					node->rect = *update;

mupdf/fitz/res_font.c
@@ -179,6 +179,20 @@ fz_finalize_freetype(void)
 	}
 }
 
+/* SumatraPDF: some Chinese fonts seem to wrongly use pre-devided units */
+static void
+fz_check_font_dimensions(FT_Face face)
+{
+	if (face->bbox.xMin == 0 && face->bbox.yMin == 0 &&
+		face->bbox.xMax == 1 && face->bbox.yMax == 1 &&
+		face->ascender == 1 && face->descender == 0)
+	{
+		face->bbox.xMax = face->units_per_EM;
+		face->bbox.yMax = face->units_per_EM;
+		face->ascender = face->units_per_EM;
+	}
+}
+
 fz_error
 fz_new_font_from_file(fz_font **fontp, char *path, int index)
 {
@@ -194,6 +208,7 @@ fz_new_font_from_file(fz_font **fontp, char *path, int index)
 	fterr = FT_New_Face(fz_ftlib, path, index, &face);
 	if (fterr)
 		return fz_throw("freetype: cannot load font: %s", ft_error_string(fterr));
+	fz_check_font_dimensions(face);
 
 	font = fz_new_font(face->family_name);
 	font->ft_face = face;
@@ -221,6 +236,7 @@ fz_new_font_from_memory(fz_font **fontp, unsigned char *data, int len, int index
 	fterr = FT_New_Memory_Face(fz_ftlib, data, len, index, &face);
 	if (fterr)
 		return fz_throw("freetype: cannot load font: %s", ft_error_string(fterr));
+	fz_check_font_dimensions(face);
 
 	font = fz_new_font(face->family_name);
 	font->ft_face = face;