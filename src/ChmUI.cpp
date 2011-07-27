kusl/sumatrapdf

commit 45790e1737079ad2b8717e354ef2b93a1aecee40

register dummy chm window procs
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4288 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/ChmUI.cpp
@@ -2,4 +2,46 @@
    License: GPLv3 */
 
 #include "ChmUI.h"
+#include "SumatraPDF.h"
+#include "resource.h"
+#include "WinUtil.h"
+
+#define FRAME_CHM_CLASS_NAME        _T("SUMATRA_PDF_CHM_FRAME")
+#define CANVAS_CHM_CLASS_NAME       _T("SUMATRA_PDF_CHM_CANVAS")
+
+static LRESULT CALLBACK WndProcChmFrame(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+static LRESULT CALLBACK WndProcChmCanvas(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+bool RegisterChmWinClass(HINSTANCE hinst)
+{
+    WNDCLASSEX  wcex;
+    ATOM        atom;
+
+    FillWndClassEx(wcex, hinst);
+    wcex.lpfnWndProc    = WndProcChmFrame;
+    wcex.lpszClassName  = FRAME_CLASS_NAME;
+    wcex.hIcon          = LoadIcon(hinst, MAKEINTRESOURCE(IDI_SUMATRAPDF));
+    wcex.hIconSm        = LoadIcon(hinst, MAKEINTRESOURCE(IDI_SMALL));
+    atom = RegisterClassEx(&wcex);
+    if (!atom)
+        return false;
+
+    FillWndClassEx(wcex, hinst);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+    wcex.lpfnWndProc    = WndProcChmCanvas;
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.lpszClassName  = CANVAS_CHM_CLASS_NAME;
+    atom = RegisterClassEx(&wcex);
+    if (!atom)
+        return false;
+
+    return true;
+}
 

src/ChmUI.h
@@ -4,5 +4,8 @@
 #ifndef ChmUI_h
 #define ChmUI_h
 
+#include "BaseUtil.h"
+
+bool RegisterChmWinClass(HINSTANCE hinst);
 
 #endif

src/SumatraPDF.cpp
@@ -34,6 +34,7 @@
 #include "ExternalPdfViewer.h"
 #include "Selection.h"
 #include "Menu.h"
+#include "ChmUI.h"
 
 #include "CrashHandler.h"
 #include "ParseCommandLine.h"
@@ -3606,10 +3607,10 @@ static LRESULT OnMouseWheel(WindowInfo& win, UINT message, WPARAM wParam, LPARAM
     return 0;
 }
 
-static LRESULT CALLBACK WndProcCanvas(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+static LRESULT CALLBACK WndProcCanvas(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     // messages that don't require win
-    switch (message) {
+    switch (msg) {
         case WM_DROPFILES:
             OnDropFiles((HDROP)wParam);
             break;
@@ -3621,10 +3622,10 @@ static LRESULT CALLBACK WndProcCanvas(HWND hwnd, UINT message, WPARAM wParam, LP
 
     WindowInfo *win = FindWindowInfoByHwnd(hwnd);
     if (!win)
-        return DefWindowProc(hwnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, msg, wParam, lParam);
 
     // messages that require win
-    switch (message) {
+    switch (msg) {
         case WM_VSCROLL:
             OnVScroll(*win, wParam);
             return WM_VSCROLL_HANDLED;
@@ -3679,7 +3680,7 @@ static LRESULT CALLBACK WndProcCanvas(HWND hwnd, UINT message, WPARAM wParam, LP
         case WM_SETCURSOR:
             if (OnSetCursor(*win, hwnd))
                 return TRUE;
-            return DefWindowProc(hwnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
 
         case WM_TIMER:
             OnTimer(*win, hwnd, wParam);
@@ -3695,13 +3696,13 @@ static LRESULT CALLBACK WndProcCanvas(HWND hwnd, UINT message, WPARAM wParam, LP
             break;
 
         case WM_MOUSEWHEEL:
-            return OnMouseWheel(*win, message, wParam, lParam);
+            return OnMouseWheel(*win, msg, wParam, lParam);
 
         default:
             // process thread queue events happening during an inner message loop
             // (else the scrolling position isn't updated until the scroll bar is released)
             gUIThreadMarshaller.Execute();
-            return DefWindowProc(hwnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
@@ -3733,7 +3734,7 @@ void WindowInfo::RepaintAsync(UINT delay)
     QueueWorkItem(new RepaintCanvasWorkItem(this, delay));
 }
 
-static LRESULT OnCommand(WindowInfo *win, HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+static LRESULT OnCommand(WindowInfo *win, HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     int wmId = LOWORD(wParam);
 
@@ -3757,7 +3758,7 @@ static LRESULT OnCommand(WindowInfo *win, HWND hwnd, UINT message, WPARAM wParam
     }
     
     if (!win)
-        return DefWindowProc(hwnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, msg, wParam, lParam);
     
     // most of them require a win, the few exceptions are no-ops without
     switch (wmId)
@@ -4024,20 +4025,20 @@ static LRESULT OnCommand(WindowInfo *win, HWND hwnd, UINT message, WPARAM wParam
             break;
 
         default:
-            return DefWindowProc(hwnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
     }
 
     return 0;
 }
 
-static LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+static LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     WindowInfo *    win;
     ULONG           ulScrollLines;                   // for mouse wheel logic
 
     win = FindWindowInfoByHwnd(hwnd);
 
-    switch (message)
+    switch (msg)
     {
         case WM_CREATE:
             // do nothing
@@ -4066,7 +4067,7 @@ static LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT message, WPARAM wParam, LPA
             break;
 
         case WM_COMMAND:
-            return OnCommand(win, hwnd, message, wParam, lParam);
+            return OnCommand(win, hwnd, msg, wParam, lParam);
 
         case WM_APPCOMMAND:
             // both keyboard and mouse drivers should produce WM_APPCOMMAND
@@ -4089,7 +4090,7 @@ static LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT message, WPARAM wParam, LPA
                 SendMessage(hwnd, WM_COMMAND, IDM_VIEW_BOOKMARKS, 0);
                 return TRUE;
             }
-            return DefWindowProc(hwnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
 
         case WM_CHAR:
             if (win)
@@ -4107,7 +4108,7 @@ static LRESULT CALLBACK WndProcFrame(HWND hwnd, UINT message, WPARAM wParam, LPA
             // handled as expected)
             if (win && GET_X_LPARAM(lParam) == -1 && GET_Y_LPARAM(lParam) == -1 && GetFocus() != win->hwndTocTree)
                 return SendMessage(win->hwndCanvas, WM_CONTEXTMENU, wParam, lParam);
-            return DefWindowProc(hwnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
 
         case WM_SETTINGCHANGE:
 InitMouseWheelInfo:
@@ -4131,12 +4132,12 @@ InitMouseWheelInfo:
             // Pass the message to the canvas' window procedure
             // (required since the canvas itself never has the focus and thus
             // never receives WM_MOUSEWHEEL messages)
-            return SendMessage(win->hwndCanvas, message, wParam, lParam);
+            return SendMessage(win->hwndCanvas, msg, wParam, lParam);
 
         case WM_DESTROY:
             /* WM_DESTROY might be sent as a result of File\Close, in which case CloseWindow() has already been called */
             if (win)
-                CloseWindow(win, TRUE, true);
+                CloseWindow(win, true, true);
             break;
 
         case WM_DDE_INITIATE:
@@ -4153,26 +4154,26 @@ InitMouseWheelInfo:
             break;
 
         default:
-            return DefWindowProc(hwnd, message, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
 
-static bool RegisterWinClass(HINSTANCE hInstance)
+static bool RegisterWinClass(HINSTANCE hinst)
 {
     WNDCLASSEX  wcex;
     ATOM        atom;
 
-    FillWndClassEx(wcex, hInstance);
+    FillWndClassEx(wcex, hinst);
     wcex.lpfnWndProc    = WndProcFrame;
     wcex.lpszClassName  = FRAME_CLASS_NAME;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SUMATRAPDF));
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.hIcon          = LoadIcon(hinst, MAKEINTRESOURCE(IDI_SUMATRAPDF));
+    wcex.hIconSm        = LoadIcon(hinst, MAKEINTRESOURCE(IDI_SMALL));
     atom = RegisterClassEx(&wcex);
     if (!atom)
         return false;
 
-    FillWndClassEx(wcex, hInstance);
+    FillWndClassEx(wcex, hinst);
     wcex.style          = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
     wcex.lpfnWndProc    = WndProcCanvas;
     wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
@@ -4181,23 +4182,23 @@ static bool RegisterWinClass(HINSTANCE hInstance)
     if (!atom)
         return false;
 
-    FillWndClassEx(wcex, hInstance);
+    FillWndClassEx(wcex, hinst);
     wcex.lpfnWndProc    = WndProcAbout;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SUMATRAPDF));
+    wcex.hIcon          = LoadIcon(hinst, MAKEINTRESOURCE(IDI_SUMATRAPDF));
     wcex.lpszClassName  = ABOUT_CLASS_NAME;
     atom = RegisterClassEx(&wcex);
     if (!atom)
         return false;
 
-    FillWndClassEx(wcex, hInstance);
+    FillWndClassEx(wcex, hinst);
     wcex.lpfnWndProc    = WndProcProperties;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SUMATRAPDF));
+    wcex.hIcon          = LoadIcon(hinst, MAKEINTRESOURCE(IDI_SUMATRAPDF));
     wcex.lpszClassName  = PROPERTIES_CLASS_NAME;
     atom = RegisterClassEx(&wcex);
     if (!atom)
         return false;
 
-    FillWndClassEx(wcex, hInstance);
+    FillWndClassEx(wcex, hinst);
     wcex.lpfnWndProc    = WndProcSpliter;
     wcex.hCursor        = LoadCursor(NULL, IDC_SIZEWE);
     wcex.hbrBackground  = CreateSolidBrush(GetSysColor(COLOR_BTNFACE));
@@ -4206,7 +4207,10 @@ static bool RegisterWinClass(HINSTANCE hInstance)
     if (!atom)
         return false;
 
-    if (!RegisterNotificationsWndClass(hInstance))
+    if (!RegisterNotificationsWndClass(hinst))
+        return false;
+
+    if (!RegisterChmWinClass(hinst))
         return false;
 
     return true;

commit 285c97c11525da32a142dbe491cd350f330f1323

make SumatraPDF start again (regression by r4288)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4292 841c7e00-0c1d-0410-a694-8ba26473ccb1

makefile.msvc
@@ -253,7 +253,11 @@ $(OS)\BencUtil.obj: src\utils\Vec.h
 $(OS)\ChmEngine.obj: src\BaseEngine.h src\ChmEngine.h src\utils\BaseUtil.h
 $(OS)\ChmEngine.obj: src\utils\FileUtil.h src\utils\GeomUtil.h src\utils\HtmlWindow.h
 $(OS)\ChmEngine.obj: src\utils\StrUtil.h src\utils\TrivialHtmlParser.h src\utils\Vec.h
-$(OS)\ChmUI.obj: src\ChmUI.h
+$(OS)\ChmUI.obj: src\AppPrefs.h src\BaseEngine.h src\ChmUI.h
+$(OS)\ChmUI.obj: src\DisplayState.h src\resource.h src\SumatraPDF.h
+$(OS)\ChmUI.obj: src\TextSearch.h src\TextSelection.h src\utils\BaseUtil.h
+$(OS)\ChmUI.obj: src\utils\GeomUtil.h src\utils\StrUtil.h src\utils\Vec.h
+$(OS)\ChmUI.obj: src\utils\WinUtil.h
 $(OS)\CPdfFilter.obj: src\BaseEngine.h src\ifilter\CPdfFilter.h src\ifilter\FilterBase.h
 $(OS)\CPdfFilter.obj: src\ifilter\PdfFilter.h src\PdfEngine.h src\utils\BaseUtil.h
 $(OS)\CPdfFilter.obj: src\utils\GeomUtil.h src\utils\StrUtil.h src\utils\Vec.h
@@ -417,20 +421,21 @@ $(OS)\SumatraDialogs.obj: src\translations.h src\utils\BaseUtil.h src\utils\Dial
 $(OS)\SumatraDialogs.obj: src\utils\GeomUtil.h src\utils\StrUtil.h src\utils\Vec.h
 $(OS)\SumatraDialogs.obj: src\utils\WinUtil.h
 $(OS)\SumatraPDF.obj: src\AppPrefs.h src\AppTools.h src\BaseEngine.h
-$(OS)\SumatraPDF.obj: src\ChmEngine.h src\CrashHandler.h src\DisplayModel.h
-$(OS)\SumatraPDF.obj: src\DisplayState.h src\DjVuEngine.h src\EngineManager.h
-$(OS)\SumatraPDF.obj: src\ExternalPdfViewer.h src\Favorites.h src\FileHistory.h
-$(OS)\SumatraPDF.obj: src\FileWatch.h src\ImagesEngine.h src\Menu.h
-$(OS)\SumatraPDF.obj: src\Notifications.h src\ParseCommandLine.h src\PdfEngine.h
-$(OS)\SumatraPDF.obj: src\PdfSync.h src\Print.h src\PsEngine.h
-$(OS)\SumatraPDF.obj: src\RenderCache.h src\resource.h src\Resource.h
-$(OS)\SumatraPDF.obj: src\Search.h src\Selection.h src\StressTesting.h
-$(OS)\SumatraPDF.obj: src\SumatraAbout.h src\SumatraDialogs.h src\SumatraPDF.h
-$(OS)\SumatraPDF.obj: src\SumatraProperties.h src\TableOfContents.h src\TextSearch.h
-$(OS)\SumatraPDF.obj: src\TextSelection.h src\Toolbar.h src\translations.h
-$(OS)\SumatraPDF.obj: src\utils\BaseUtil.h src\utils\FileUtil.h src\utils\GeomUtil.h
-$(OS)\SumatraPDF.obj: src\utils\Http.h src\utils\StrUtil.h src\utils\Vec.h
-$(OS)\SumatraPDF.obj: src\utils\WinUtil.h src\Version.h src\WindowInfo.h
+$(OS)\SumatraPDF.obj: src\ChmEngine.h src\ChmUI.h src\CrashHandler.h
+$(OS)\SumatraPDF.obj: src\DisplayModel.h src\DisplayState.h src\DjVuEngine.h
+$(OS)\SumatraPDF.obj: src\EngineManager.h src\ExternalPdfViewer.h src\Favorites.h
+$(OS)\SumatraPDF.obj: src\FileHistory.h src\FileWatch.h src\ImagesEngine.h
+$(OS)\SumatraPDF.obj: src\Menu.h src\Notifications.h src\ParseCommandLine.h
+$(OS)\SumatraPDF.obj: src\PdfEngine.h src\PdfSync.h src\Print.h
+$(OS)\SumatraPDF.obj: src\PsEngine.h src\RenderCache.h src\resource.h
+$(OS)\SumatraPDF.obj: src\Resource.h src\Search.h src\Selection.h
+$(OS)\SumatraPDF.obj: src\StressTesting.h src\SumatraAbout.h src\SumatraDialogs.h
+$(OS)\SumatraPDF.obj: src\SumatraPDF.h src\SumatraProperties.h src\TableOfContents.h
+$(OS)\SumatraPDF.obj: src\TextSearch.h src\TextSelection.h src\Toolbar.h
+$(OS)\SumatraPDF.obj: src\translations.h src\utils\BaseUtil.h src\utils\FileUtil.h
+$(OS)\SumatraPDF.obj: src\utils\GeomUtil.h src\utils\Http.h src\utils\StrUtil.h
+$(OS)\SumatraPDF.obj: src\utils\Vec.h src\utils\WinUtil.h src\Version.h
+$(OS)\SumatraPDF.obj: src\WindowInfo.h
 $(OS)\SumatraProperties.obj: src\AppPrefs.h src\BaseEngine.h src\ChmEngine.h
 $(OS)\SumatraProperties.obj: src\DisplayModel.h src\DisplayState.h src\DjVuEngine.h
 $(OS)\SumatraProperties.obj: src\EngineManager.h src\ImagesEngine.h src\PdfEngine.h

src/ChmUI.cpp
@@ -24,6 +24,7 @@ bool RegisterChmWinClass(HINSTANCE hinst)
     WNDCLASSEX  wcex;
     ATOM        atom;
 
+    /* TODO: can't register FRAME_CLASS_NAME twice
     FillWndClassEx(wcex, hinst);
     wcex.lpfnWndProc    = WndProcChmFrame;
     wcex.lpszClassName  = FRAME_CLASS_NAME;
@@ -32,6 +33,7 @@ bool RegisterChmWinClass(HINSTANCE hinst)
     atom = RegisterClassEx(&wcex);
     if (!atom)
         return false;
+    */
 
     FillWndClassEx(wcex, hinst);
     wcex.style          = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
@@ -44,4 +46,3 @@ bool RegisterChmWinClass(HINSTANCE hinst)
 
     return true;
 }
-