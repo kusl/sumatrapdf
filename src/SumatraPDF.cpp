kusl/sumatrapdf

commit fe5608c96fb26697d693434d0278654c27ccddb9

sometimes use a notification when trying to open an inexistent file (fixes issue 1214)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4183 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/SumatraPDF.cpp
@@ -1503,7 +1503,25 @@ WindowInfo* LoadDocument(const TCHAR *fileName, WindowInfo *win, bool showWin, b
 
     ScopedMem<TCHAR> fullpath(path::Normalize(fileName));
     if (!fullpath)
-        return win;
+        return NULL;
+
+    // fail with a notification if the file doesn't exist and
+    // there is a window the user has just been interacting with
+    if (win && !forceReuse && !file::Exists(fullpath)) {
+        // TODO: reword to "File %s not found!" after 1.7 is released
+        ScopedMem<TCHAR> msg(str::Format(_TR("Error loading %s"), fullpath));
+        ShowNotification(win, msg, true /* autoDismiss */, true /* highlight */);
+        // display the notification ASAP (SavePrefs() can introduce a notable delay)
+        win->RedrawAll(true);
+
+        if (gFileHistory.MarkFileInexistent(fullpath)) {
+            SavePrefs();
+            // update the Frequently Read list
+            if (1 == gWindows.Count() && gWindows[0]->IsAboutWindow())
+                gWindows[0]->RedrawAll(true);
+        }
+        return NULL;
+    }
 
     bool isNewWindow = false;
     if (!win && 1 == gWindows.Count() && gWindows[0]->IsAboutWindow()) {
@@ -2291,9 +2309,9 @@ static void OnMouseLeftButtonUp(WindowInfo& win, int x, int y, WPARAM key)
                 win.RedrawAll(true);
             } else if (!str::StartsWithI(url, _T("http:")) &&
                        !str::StartsWithI(url, _T("https:")) &&
-                       !str::StartsWithI(url, _T("mailto:")))
+                       !str::StartsWithI(url, _T("mailto:"))) {
                 LoadDocument(url, &win);
-            else
+            } else
                 LaunchBrowser(url);
         }
         win.url = NULL;

src/SumatraPDF.h
@@ -98,4 +98,10 @@ HMENU BuildMenuFromMenuDef(MenuDef menuDefs[], int menuLen, HMENU menu);
 void  ReloadDocument(WindowInfo *win, bool autorefresh=false);
 void  PaintTransparentRectangle(HDC hdc, RectI screenRc, RectI *rect, COLORREF selectionColor, BYTE alpha = 0x5f, int margin = 1);
 
+WindowInfo* FindWindowInfoByFile(TCHAR *file);
+WindowInfo* FindWindowInfoByHwnd(HWND hwnd);
+WindowInfo* FindWindowInfoBySyncFile(TCHAR *file);
+WindowInfo* LoadDocument(const TCHAR *fileName, WindowInfo *win=NULL,
+                         bool showWin=true, bool forceReuse=false, bool suppressPwdUI=false);
+
 #endif

src/WindowInfo.cpp
@@ -195,7 +195,7 @@ void LinkHandler::GotoLink(PageDestination *link)
     else if (str::Eq(type, "LaunchEmbedded")) {
         // open embedded PDF documents in a new window
         if (path && str::StartsWith(path.Get(), dm->FileName()))
-            LoadDocument(path);
+            LoadDocument(path, owner);
         // offer to save other attachments to a file
         else
             link->SaveEmbedded(LinkSaver(owner->hwndFrame, path));
@@ -207,7 +207,7 @@ void LinkHandler::GotoLink(PageDestination *link)
             ScopedMem<TCHAR> basePath(path::GetDir(dm->FileName()));
             ScopedMem<TCHAR> combinedPath(path::Join(basePath, path));
             // TODO: respect fz_to_bool(fz_dict_gets(link->dest, "NewWindow")) for ScrollToEx
-            WindowInfo *newWin = LoadDocument(combinedPath);
+            WindowInfo *newWin = LoadDocument(combinedPath, owner);
 
             if (str::Eq(type, "ScrollToEx") && newWin && newWin->IsDocLoaded())
                 newWin->linkHandler->ScrollTo(link);

src/WindowInfo.h
@@ -228,10 +228,4 @@ public:
     virtual bool SaveEmbedded(unsigned char *data, int cbCount);
 };
 
-WindowInfo* FindWindowInfoByFile(TCHAR *file);
-WindowInfo* FindWindowInfoByHwnd(HWND hwnd);
-WindowInfo* FindWindowInfoBySyncFile(TCHAR *file);
-WindowInfo* LoadDocument(const TCHAR *fileName, WindowInfo *win=NULL,
-                         bool showWin=true, bool forceReuse=false, bool suppressPwdUI=false);
-
 #endif

sumatrapdf-vc2008.vcproj
@@ -211,6 +211,10 @@
 					>
 				</File>
 				<File
+					RelativePath=".\src\Notifications.cpp"
+					>
+				</File>
+				<File
 					RelativePath=".\src\Notifications.h"
 					>
 				</File>

commit 6fb36e2d8d28800d7f9eac0024278af8b802ce1f

fix opening of embedded documents (regression from r4183)
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@4342 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/SumatraPDF.cpp
@@ -990,7 +990,9 @@ WindowInfo* LoadDocument(const TCHAR *fileName, WindowInfo *win, bool showWin, b
 
     // fail with a notification if the file doesn't exist and
     // there is a window the user has just been interacting with
-    if (win && !forceReuse && !file::Exists(fullpath)) {
+    // Note: embedded documents are referred to by an invalid path
+    //       containing more information after a colon (e.g. "C:\file.pdf:3:0")
+    if (win && !forceReuse && !file::Exists(fullpath) && !str::FindChar(fullpath + 2, ':')) {
         // TODO: reword to "File %s not found!" after 1.7 is released
         ScopedMem<TCHAR> msg(str::Format(_TR("Error loading %s"), fullpath));
         ShowNotification(win, msg, true /* autoDismiss */, true /* highlight */);