kusl/sumatrapdf

commit 810d9d6e1f68663b1ef45b79c32698bbbddd53b0

extract images for FB2 files, expose ImageData from BaseEbookDoc
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5444 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/BaseEbookDoc.h
@@ -4,6 +4,15 @@
 #ifndef BaseEbookDoc_h
 #define BaseEbookDoc_h
 
+#include "BaseUtil.h"
+
+// TODO: use Gdiplus::Bitmap instead?
+struct ImageData {
+    char *  data;
+    size_t  len;
+    char *  id;
+};
+
 class BaseEbookDoc {
 public:
     virtual ~BaseEbookDoc() { }
@@ -11,6 +20,12 @@ public:
     // a single HTML stream for further processing/layout
     // the result is owned by the class, don't free
     virtual const char *GetBookHtmlData(size_t& lenOut) = 0;
+    // returns the data for an image by ID
+    // the result is owned by the class, don't free
+    virtual ImageData *GetImageData(const char *id) { return NULL; }
+    // returns the data for an image by (arbitrary) index
+    // the result is owned by the class, don't free
+    virtual ImageData *GetImageData(size_t index) { return NULL; }
 };
 
 #endif

src/Fb2Doc.cpp
@@ -26,30 +26,39 @@ class CFb2Doc : public Fb2Doc {
 
     ScopedMem<TCHAR> fileName;
     str::Str<char> htmlData;
+    Vec<ImageData> images;
 
     bool Load();
+    void ExtractImage(HtmlPullParser& parser, HtmlToken *tok);
 
 public:
     CFb2Doc(const TCHAR *fileName) : fileName(str::Dup(fileName)) { }
+    virtual ~CFb2Doc() {
+        for (size_t i = 0; i < images.Count(); i++) {
+            free(images.At(i).data);
+            free(images.At(i).id);
+        }
+    }
 
     virtual const char *GetBookHtmlData(size_t& lenOut) {
         lenOut = htmlData.Size();
         return htmlData.Get();
     }
+
+    virtual ImageData *GetImageData(const char *id) {
+        for (size_t i = 0; i < images.Count(); i++) {
+            if (str::Eq(images.At(i).id, id))
+                return GetImageData(i);
+        }
+        return NULL;
+    }
+
+    virtual ImageData *GetImageData(size_t index) {
+        if (index > images.Count())
+            return NULL;
+        return &images.At(index);
+    }
 };
-
-inline bool IsFb2Tag(HtmlToken *tok, const char *tag)
-{
-    size_t len = str::Len(tag);
-    return GetTagLen(tok->s, tok->sLen) == len &&
-           str::EqN(tok->s, tag, len);
-}
-
-inline bool IsFb2Attr(AttrInfo *attrInfo, const char *name)
-{
-    size_t len = str::Len(name);
-    return len == attrInfo->nameLen && str::EqN(attrInfo->name, name, len);
-}
 
 bool CFb2Doc::Load()
 {
@@ -75,6 +84,8 @@ bool CFb2Doc::Load()
             inBody++;
         else if (inBody && tok->IsEndTag() && Tag_Body == tag)
             inBody--;
+        if (!inBody && tok->IsStartTag() && tok->HasName("binary"))
+            ExtractImage(parser, tok);
         if (!inBody)
             continue;
 
@@ -100,13 +111,13 @@ bool CFb2Doc::Load()
             else if (Tag_Strong == tag) {
                 htmlData.Append("<strong>");
             }
-            else if (IsFb2Tag(tok, "section")) {
+            else if (tok->HasName("section")) {
                 sectionDepth++;
             }
-            else if (IsFb2Tag(tok, "emphasis")) {
+            else if (tok->HasName("emphasis")) {
                 htmlData.Append("<em>");
             }
-            else if (IsFb2Tag(tok, "epigraph")) {
+            else if (tok->HasName("epigraph")) {
                 htmlData.Append("<blockquote>");
             }
             // TODO: handle Tag_A, <text-author>
@@ -126,32 +137,96 @@ bool CFb2Doc::Load()
             else if (Tag_Strong == tag) {
                 htmlData.Append("</strong>");
             }
-            else if (IsFb2Tag(tok, "section") && sectionDepth > 0) {
+            else if (tok->HasName("section") && sectionDepth > 0) {
                 sectionDepth--;
             }
-            else if (IsFb2Tag(tok, "emphasis")) {
+            else if (tok->HasName("emphasis")) {
                 htmlData.Append("</em>");
             }
-            else if (IsFb2Tag(tok, "epigraph")) {
+            else if (tok->HasName("epigraph")) {
                 htmlData.Append("</blockquote>");
             }
         }
         else if (tok->IsEmptyElementEndTag()) {
-            if (IsFb2Tag(tok, "image")) {
+            if (tok->HasName("image")) {
                 AttrInfo *attrInfo;
                 while ((attrInfo = tok->NextAttr())) {
-                    if (IsFb2Attr(attrInfo, "xlink:href")) {
+                    if (attrInfo->HasName("xlink:href")) {
                         ScopedMem<char> link(str::DupN(attrInfo->val, attrInfo->valLen));
+                        htmlData.AppendFmt("<img src=\"%s\">", link);
                     }
                 }
             }
-            else if (IsFb2Tag(tok, "empty-line")) {
+            else if (tok->HasName("empty-line")) {
                 htmlData.Append("<p></p>");
             }
         }
     }
 
     return htmlData.Count() > 0;
+}
+
+inline char decode64(char c)
+{
+    if ('A' <= c && c <= 'Z')
+        return c - 'A';
+    if ('a' <= c && c <= 'z')
+        return c - 'a' + 26;
+    if ('0' <= c && c <= '9')
+        return c - '0' + 52;
+    if ('+' == c)
+        return 62;
+    if ('/' == c)
+        return 63;
+    return -1;
+}
+
+char *Base64Decode(const char *s, const char *end, size_t *len)
+{
+    size_t bound = (end - s) * 3 / 4;
+    char *result = SAZA(char, bound);
+    char *curr = result;
+    unsigned char c = 0;
+    int step = 0;
+    for (; s < end && *s != '='; s++) {
+        char n = decode64(*s);
+        if (-1 == n) {
+            if (isspace(*s))
+                continue;
+            free(result);
+            return NULL;
+        }
+        switch (step++ % 4) {
+        case 0: c = n; break;
+        case 1: *curr++ = (c << 2) | (n >> 4); c = n & 0xF; break;
+        case 2: *curr++ = (c << 4) | (n >> 2); c = n & 0x3; break;
+        case 3: *curr++ = (c << 6) | (n >> 0); break;
+        }
+    }
+    if (len)
+        *len = curr - result;
+    return result;
+}
+
+void CFb2Doc::ExtractImage(HtmlPullParser& parser, HtmlToken *tok)
+{
+    ScopedMem<char> id;
+    AttrInfo *attrInfo;
+    while ((attrInfo = tok->NextAttr())) {
+        if (attrInfo->HasName("id"))
+            id.Set(str::DupN(attrInfo->val, attrInfo->valLen));
+    }
+
+    tok = parser.Next();
+    if (!tok || !tok->IsText())
+        return;
+
+    ImageData data;
+    data.data = Base64Decode(tok->s, tok->s + tok->sLen, &data.len);
+    if (!data.data)
+        return;
+    data.id = str::Join("#", id);
+    images.Append(data);
 }
 
 Fb2Doc *Fb2Doc::ParseFile(const TCHAR *fileName)

src/MobiDoc.cpp
@@ -1,16 +1,129 @@
 /* Copyright 2011-2012 the SumatraPDF project authors (see AUTHORS file).
    License: Simplified BSD (see COPYING.BSD) */
 
-#include "MobiDoc.h"
-#include <time.h>
-
+#include "BaseUtil.h"
 #include "BitReader.h"
 #include "FileUtil.h"
-
 #include "StrUtil.h"
+#include "Vec.h"
+#include "Scoped.h"
+
+#include "MobiDoc.h"
 
 using namespace Gdiplus;
 #include "GdiPlusUtil.h"
+
+class HuffDicDecompressor;
+
+// http://en.wikipedia.org/wiki/PDB_(Palm_OS)
+#define kDBNameLength    32
+#define kPdbHeaderLen    78
+
+#pragma pack(push)
+#pragma pack(1)
+struct PdbHeader
+{
+     /* 31 chars + 1 null terminator */
+    char        name[kDBNameLength];
+    uint16      attributes;
+    uint16      version;
+    uint32      createTime;
+    uint32      modifyTime;
+    uint32      backupTime;
+    uint32      modificationNumber;
+    uint32      appInfoID;
+    uint32      sortInfoID;
+    char        type[4];
+    char        creator[4];
+    uint32      idSeed;
+    uint32      nextRecordList;
+    uint16      numRecords;
+};
+#pragma pack(pop)
+
+STATIC_ASSERT(kPdbHeaderLen == sizeof(PdbHeader), validPdbHeader);
+
+#define kPdbRecordHeaderLen 8
+
+#pragma pack(push)
+#pragma pack(1)
+struct PdbRecordHeader {
+    uint32   offset;
+    uint8    deleted   : 1;
+    uint8    dirty     : 1;
+    uint8    busy      : 1;
+    uint8    secret    : 1;
+    uint8    category  : 4;
+    char     uniqueID[3];
+};
+#pragma pack(pop)
+
+STATIC_ASSERT(kPdbRecordHeaderLen == sizeof(PdbRecordHeader), validPdbRecordHeader);
+
+#define kMaxRecordSize 64*1024
+
+class CMobiDoc : public MobiDoc {
+    friend MobiDoc;
+
+    ScopedMem<TCHAR>    fileName;
+    HANDLE              fileHandle;
+
+    PdbHeader           pdbHeader;
+    PdbRecordHeader *   recHeaders;
+    char *              firstRecData;
+
+    str::Str<char> *    doc;
+
+    bool                isMobi;
+    size_t              docRecCount;
+    int                 compressionType;
+    size_t              docUncompressedSize;
+
+    bool                multibyte;
+    size_t              trailersCount;
+    size_t              imageFirstRec; // 0 if no images
+
+    Vec<ImageData>      images;
+
+    // we use bufStatic if record fits in it, bufDynamic otherwise
+    char                bufStatic[kMaxRecordSize];
+    char *              bufDynamic;
+    size_t              bufDynamicSize;
+
+    HuffDicDecompressor *huffDic;
+
+    bool    Load();
+    bool    LoadDocument();
+    bool    ParseHeader();
+    char *  GetBufForRecordData(size_t size);
+    size_t  GetRecordSize(size_t recNo);
+    char*   ReadRecord(size_t recNo, size_t& sizeOut);
+    bool    LoadDocRecordIntoBuffer(size_t recNo, str::Str<char>& strOut);
+    void    LoadImages(size_t imagesCount);
+    bool    LoadImage(size_t imageNo);
+
+public:
+    CMobiDoc(const TCHAR *fileName);
+    virtual ~CMobiDoc();
+
+    virtual const char *GetBookHtmlData(size_t& lenOut) {
+        lenOut = doc->Size();
+        return doc->Get();
+    }
+
+    virtual ImageData *GetImageData(size_t index) {
+        if (index > images.Count())
+            return NULL;
+        if (!images.At(index).data)
+            return NULL;
+        return &images.At(index);
+    }
+
+    virtual ImageData *GetImageData(const char *id) {
+        int idx = atoi(id);
+        return GetImageData(idx);
+    }
+};
 
 // Parse mobi format http://wiki.mobileread.com/wiki/MOBI
 
@@ -519,27 +632,30 @@ static bool IsValidCompression(int comprType)
             (COMPRESSION_HUFF == comprType);
 }
 
-MobiDoc::MobiDoc() :
-    fileName(NULL), fileHandle(0), recHeaders(NULL), firstRecData(NULL), isMobi(false),
+CMobiDoc::CMobiDoc(const TCHAR *fileName) : fileName(str::Dup(fileName)),
+    fileHandle(NULL), recHeaders(NULL), firstRecData(NULL), isMobi(false),
     docRecCount(0), compressionType(0), docUncompressedSize(0), doc(NULL),
-    multibyte(false), trailersCount(0), imageFirstRec(0), imagesCount(0), validImagesCount(0),
+    multibyte(false), trailersCount(0), imageFirstRec(0),
     images(NULL), bufDynamic(NULL), bufDynamicSize(0), huffDic(NULL)
 {
 }
 
-MobiDoc::~MobiDoc()
+CMobiDoc::~CMobiDoc()
 {
     CloseHandle(fileHandle);
     free(fileName);
     free(firstRecData);
     free(recHeaders);
     free(bufDynamic);
-    free(images);
+    for (size_t i = 0; i < images.Count(); i++) {
+        free(images.At(i).data);
+        free(images.At(i).id);
+    }
     delete huffDic;
     delete doc;
 }
 
-bool MobiDoc::ParseHeader()
+bool CMobiDoc::ParseHeader()
 {
     DWORD bytesRead;
     BOOL ok = ReadFile(fileHandle, (void*)&pdbHeader, kPdbHeaderLen, &bytesRead, NULL);
@@ -653,12 +769,11 @@ bool MobiDoc::ParseHeader()
     SwapU32(mobiHdr->huffmanTableLen);
     SwapU32(mobiHdr->exhtFlags);
 
+    size_t imagesCount = 0;
     if (pdbHeader.numRecords > mobiHdr->imageFirstRec) {
         imageFirstRec = mobiHdr->imageFirstRec;
-        if (0 == imageFirstRec) {
-            // I don't think this should ever happen but I've seen it
-            imagesCount = 0;
-        } else
+        // I don't think this should ever happen but I've seen it
+        if (imageFirstRec != 0)
             imagesCount = pdbHeader.numRecords - mobiHdr->imageFirstRec;
     }
     size_t hdrLen = mobiHdr->hdrLen;
@@ -704,7 +819,7 @@ bool MobiDoc::ParseHeader()
         }
     }
 
-    LoadImages();
+    LoadImages(imagesCount);
     return true;
 }
 
@@ -728,78 +843,77 @@ static uint32 GetUpToFour(uint8*& s, size_t& len)
     return v;
 }
 
-static bool IsEofRecord(uint8 *data, size_t dataLen)
+inline bool IsEofRecord(char *data, size_t len)
 {
-    return (4 == dataLen) && (EOF_REC == GetUpToFour(data, dataLen));
+    uint8 *data2 = (uint8 *)data;
+    return (4 == len) && (EOF_REC == GetUpToFour(data2, len));
 }
 
-static bool KnownNonImageRec(uint8 *data, size_t dataLen)
+static bool KnownNonImageRec(char *data, size_t len)
 {
-    uint32 sig = GetUpToFour(data, dataLen);
+    uint8 *data2 = (uint8 *)data;
+    uint32 sig = GetUpToFour(data2, len);
 
-    if (FLIS_REC == sig) return true;
-    if (FCIS_REC == sig) return true;
-    if (FDST_REC == sig) return true;
-    if (DATP_REC == sig) return true;
-    if (SRCS_REC == sig) return true;
-    if (VIDE_REC == sig) return true;
-    return false;
+    switch (sig) {
+    case FLIS_REC: case FCIS_REC: case FDST_REC:
+    case DATP_REC: case SRCS_REC: case VIDE_REC:
+        return true;
+    default:
+        return false;
+    }
 }
 
-static bool KnownImageFormat(uint8 *data, size_t dataLen)
+static bool KnownImageFormat(ImageData& data)
 {
-    return NULL != GfxFileExtFromData((char*)data, dataLen);
+    return NULL != GfxFileExtFromData(data.data, data.len);
 }
 
 // return false if we should stop loading images (because we
 // encountered eof record or ran out of memory)
-bool MobiDoc::LoadImage(size_t imageNo)
+bool CMobiDoc::LoadImage(size_t imageNo)
 {
-    size_t imageRec = imageFirstRec + imageNo;
-    images[imageNo].imgData = 0;
-    images[imageNo].imgDataLen = 0;
-    size_t imgDataLen;
+    ImageData imgData = { 0 };
 
-    uint8 *imgData = (uint8*)ReadRecord(imageRec, imgDataLen);
-    if (!imgData)
-        return true;
-    if (IsEofRecord(imgData, imgDataLen))
-        return false;
-    if (KnownNonImageRec(imgData, imgDataLen)) {
-        imgData[5] = 0;
+    size_t len;
+    char *data = ReadRecord(imageFirstRec + imageNo, len);
+    if (!data) {
+        images.Append(imgData);
         return true;
     }
-    if (!KnownImageFormat(imgData, imgDataLen)) {
+    if (IsEofRecord(data, len))
+        return false;
+    if (KnownNonImageRec(data, len)) {
+        data[5] = 0; // TODO: ???
+        images.Append(imgData);
+        return true;
+    }
+    imgData.len = len;
+    imgData.data = (char *)memdup(data, len);
+    imgData.id = NULL; // recindex
+    if (!imgData.data)
+        return false;
+    if (!KnownImageFormat(imgData))
         l("Unknown image format\n");
-        return true;
-    }
-    images[imageNo].imgData = (uint8*)memdup(imgData, imgDataLen);
-    if (!images[imageNo].imgData)
-        return false;
-    images[imageNo].imgDataLen = imgDataLen;
-    ++validImagesCount;
+    images.Append(imgData);
     return true;
 }
 
-void MobiDoc::LoadImages()
+void CMobiDoc::LoadImages(size_t imagesCount)
 {
-    if (0 == imagesCount)
-        return;
-    images = SAZA(ImageData, imagesCount);
     for (size_t i = 0; i < imagesCount; i++) {
         if (!LoadImage(i))
             return;
     }
 }
 
-size_t MobiDoc::GetRecordSize(size_t recNo)
+size_t CMobiDoc::GetRecordSize(size_t recNo)
 {
     size_t size = recHeaders[recNo + 1].offset - recHeaders[recNo].offset;
     return size;
 }
 
 // returns NULL if error (failed to allocated)
-char *MobiDoc::GetBufForRecordData(size_t size)
+char *CMobiDoc::GetBufForRecordData(size_t size)
 {
     if (size <= sizeof(bufStatic))
         return bufStatic;
@@ -812,7 +926,7 @@ char *MobiDoc::GetBufForRecordData(size_t size)
 }
 
 // read a record and return it's data and size. Return NULL if error
-char* MobiDoc::ReadRecord(size_t recNo, size_t& sizeOut)
+char* CMobiDoc::ReadRecord(size_t recNo, size_t& sizeOut)
 {
     size_t off = recHeaders[recNo].offset;
     DWORD toRead = GetRecordSize(recNo);
@@ -860,7 +974,7 @@ static size_t ExtraDataSize(uint8 *recData, size_t recLen, size_t trailersCount,
 
 // Load a given record of a document into strOut, uncompressing if necessary.
 // Returns false if error.
-bool MobiDoc::LoadDocRecordIntoBuffer(size_t recNo, str::Str<char>& strOut)
+bool CMobiDoc::LoadDocRecordIntoBuffer(size_t recNo, str::Str<char>& strOut)
 {
     size_t recSize;
     char *recData = ReadRecord(recNo, recSize);
@@ -903,7 +1017,7 @@ bool MobiDoc::LoadDocRecordIntoBuffer(size_t recNo, str::Str<char>& strOut)
 }
 
 // assumes that ParseHeader() has been called
-bool MobiDoc::LoadDocument()
+bool CMobiDoc::LoadDocument()
 {
     assert(docUncompressedSize > 0);
     assert(!doc);
@@ -916,23 +1030,25 @@ bool MobiDoc::LoadDocument()
     return true;
 }
 
+bool CMobiDoc::Load()
+{
+    fileHandle = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ, NULL,
+                            OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (INVALID_HANDLE_VALUE == fileHandle)
+        return false;
+    if (!ParseHeader())
+        return false;
+    return LoadDocument();
+}
+
 MobiDoc *MobiDoc::ParseFile(const TCHAR *fileName)
 {
-    HANDLE fh = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ, NULL,
-                              OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (fh == INVALID_HANDLE_VALUE)
-        return NULL;
-    MobiDoc *mb = new MobiDoc();
-    mb->fileName = str::Dup(fileName);
-    mb->fileHandle = fh;
-    if (!mb->ParseHeader())
-        goto Error;
-    if (!mb->LoadDocument())
-        goto Error;
+    CMobiDoc *mb = new CMobiDoc(fileName);
+    if (!mb || !mb->Load()) {
+        delete mb;
+        mb = NULL;
+    }
     return mb;
-Error:
-    delete mb;
-    return NULL;
 }
 
 bool MobiDoc::IsSupported(const TCHAR *fileName)

src/Tester.cpp
@@ -56,20 +56,21 @@ static void SaveMobiHtml(const TCHAR *filePathBase, MobiDoc *mb)
 static void SaveMobiImage(const TCHAR *filePathBase, size_t imgNo, ImageData *img)
 {
     // it's valid to not have image data at a given index
-    if (!img->imgData)
+    if (!img->data)
         return;
-    const TCHAR *ext = GfxFileExtFromData((char*)img->imgData, img->imgDataLen);
+    const TCHAR *ext = GfxFileExtFromData(img->data, img->len);
     CrashAlwaysIf(!ext);
     ScopedMem<TCHAR> fileName(str::Format(_T("%s_img_%d%s"), filePathBase, imgNo, ext));
-    file::WriteAll(fileName.Get(), img->imgData, img->imgDataLen);
+    file::WriteAll(fileName.Get(), img->data, img->len);
 }
 
 static void SaveMobiImages(const TCHAR *filePathBase, MobiDoc *mb)
 {
     if (!gMobiSaveImages)
         return;
-    for (size_t i = 0; i < mb->imagesCount; i++) {
-        SaveMobiImage(filePathBase, i, mb->images + i);
+    ImageData *image;
+    for (size_t i = 0; (image = mb->GetImageData(i)); i++) {
+        SaveMobiImage(filePathBase, i, image);
     }
 }
 

src/MobiDoc.h
@@ -4,114 +4,10 @@
 #ifndef MobiDoc_h
 #define MobiDoc_h
 
-#include "BaseUtil.h"
-#include "Vec.h"
 #include "BaseEbookDoc.h"
 
-class HuffDicDecompressor;
-
-// http://en.wikipedia.org/wiki/PDB_(Palm_OS)
-#define kDBNameLength    32
-#define kPdbHeaderLen    78
-
-#pragma pack(push)
-#pragma pack(1)
-struct PdbHeader
-{
-     /* 31 chars + 1 null terminator */
-    char        name[kDBNameLength];
-    uint16      attributes;
-    uint16      version;
-    uint32      createTime;
-    uint32      modifyTime;
-    uint32      backupTime;
-    uint32      modificationNumber;
-    uint32      appInfoID;
-    uint32      sortInfoID;
-    char        type[4];
-    char        creator[4];
-    uint32      idSeed;
-    uint32      nextRecordList;
-    uint16      numRecords;
-};
-#pragma pack(pop)
-
-STATIC_ASSERT(kPdbHeaderLen == sizeof(PdbHeader), validPdbHeader);
-
-#define kPdbRecordHeaderLen 8
-
-#pragma pack(push)
-#pragma pack(1)
-struct PdbRecordHeader {
-    uint32   offset;
-    uint8    deleted   : 1;
-    uint8    dirty     : 1;
-    uint8    busy      : 1;
-    uint8    secret    : 1;
-    uint8    category  : 4;
-    char     uniqueID[3];
-};
-#pragma pack(pop)
-
-STATIC_ASSERT(kPdbRecordHeaderLen == sizeof(PdbRecordHeader), validPdbRecordHeader);
-
-#define kMaxRecordSize 64*1024
-
-struct ImageData {
-    uint8 *     imgData;
-    size_t      imgDataLen;
-};
-
-class MobiDoc : public BaseEbookDoc
-{
-    TCHAR *             fileName;
-    HANDLE              fileHandle;
-
-    PdbHeader           pdbHeader;
-    PdbRecordHeader *   recHeaders;
-    char *              firstRecData;
-
-    bool                isMobi;
-    size_t              docRecCount;
-    int                 compressionType;
-    size_t              docUncompressedSize;
-
-    bool                multibyte;
-    size_t              trailersCount;
-    size_t              imageFirstRec; // 0 if no images
-
-    // we use bufStatic if record fits in it, bufDynamic otherwise
-    char                bufStatic[kMaxRecordSize];
-    char *              bufDynamic;
-    size_t              bufDynamicSize;
-
-    HuffDicDecompressor *huffDic;
-
-    MobiDoc();
-
-    bool LoadDocument();
-    bool    ParseHeader();
-    char *  GetBufForRecordData(size_t size);
-    size_t  GetRecordSize(size_t recNo);
-    char*   ReadRecord(size_t recNo, size_t& sizeOut);
-    bool    LoadDocRecordIntoBuffer(size_t recNo, str::Str<char>& strOut);
-    void    LoadImages();
-    bool    LoadImage(size_t imageNo);
-
+class MobiDoc : public BaseEbookDoc {
 public:
-    str::Str<char> *    doc;
-
-    size_t              imagesCount;
-    size_t              validImagesCount;
-    ImageData *         images;
-
-    virtual const char *GetBookHtmlData(size_t& lenOut) {
-        lenOut = doc->Size();
-        return doc->Get();
-    }
-
-    virtual ~MobiDoc();
-
     static MobiDoc *ParseFile(const TCHAR *fileName);
     static bool IsSupported(const TCHAR *fileName);
 };

src/utils/HtmlPullParser.cpp
@@ -138,6 +138,7 @@ Next:
     // '<' - tag begins
     ++start;
 
+    // TODO: this will be confused by <tag attr=">" />
     if (!SkipUntil(currPos, end, '>')) {
         currToken.SetError(HtmlToken::UnclosedTag, start);
         return &currToken;

src/utils/HtmlPullParser.h
@@ -117,7 +117,13 @@ struct AttrInfo {
     size_t            nameLen;
     const char *      val;
     size_t            valLen;
+
+    bool HasName(const char *s) {
+        return str::Len(s) == nameLen && str::StartsWith(name, s);
+    }
 };
+
+size_t GetTagLen(const char *s, size_t len);
 
 struct HtmlToken {
     enum TokenType {
@@ -159,6 +165,10 @@ struct HtmlToken {
     const char *     s;
     size_t           sLen;
 
+    bool HasName(const char *name) {
+        return str::StartsWith(s, name) && str::Len(name) == GetTagLen(s, sLen);
+    }
+
     AttrInfo *       NextAttr();
 protected:
     const char *     nextAttr;
@@ -198,7 +208,6 @@ void        SkipWs(const char*& s, const char *end);
 void        SkipNonWs(const char*& s, const char *end);
 int         FindStrPos(const char *strings, const char *str, size_t len);
 
-size_t      GetTagLen(const char *s, size_t len);
 bool        IsSelfClosingTag(HtmlTag tag);
 
 HtmlTag     FindTag(const char *tag, size_t len);

commit 3789612a71f4b9904de89b20c13458db8955fb73

fix double-free error introduced with r5444
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5447 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/BaseEbookDoc.h
@@ -16,6 +16,9 @@ struct ImageData {
 class BaseEbookDoc {
 public:
     virtual ~BaseEbookDoc() { }
+    // returns the document's path
+    // the result is owned by the class, don't free
+    virtual const TCHAR *GetFilepath() = 0;
     // returns the document's content converted into
     // a single HTML stream for further processing/layout
     // the result is owned by the class, don't free

src/EpubDoc.cpp
@@ -27,11 +27,16 @@ class CEpubDoc : public EpubDoc {
     ScopedMem<TCHAR> fileName;
     ZipFile zip;
     str::Str<char> htmlData;
+    Vec<ImageData> images;
 
     bool Load();
 
 public:
     CEpubDoc(const TCHAR *fileName) : zip(fileName), fileName(str::Dup(fileName)) { }
+
+    virtual const TCHAR *GetFilepath() {
+        return fileName;
+    }
 
     virtual const char *GetBookHtmlData(size_t& lenOut) {
         lenOut = htmlData.Size();

src/Fb2Doc.cpp
@@ -40,6 +40,10 @@ public:
         }
     }
 
+    virtual const TCHAR *GetFilepath() {
+        return fileName;
+    }
+
     virtual const char *GetBookHtmlData(size_t& lenOut) {
         lenOut = htmlData.Size();
         return htmlData.Get();

src/MobiDoc.cpp
@@ -72,7 +72,7 @@ class CMobiDoc : public MobiDoc {
     PdbRecordHeader *   recHeaders;
     char *              firstRecData;
 
-    str::Str<char> *    doc;
+    str::Str<char>      doc;
 
     bool                isMobi;
     size_t              docRecCount;
@@ -106,9 +106,13 @@ public:
     CMobiDoc(const TCHAR *fileName);
     virtual ~CMobiDoc();
 
+    virtual const TCHAR *GetFilepath() {
+        return fileName;
+    }
+
     virtual const char *GetBookHtmlData(size_t& lenOut) {
-        lenOut = doc->Size();
-        return doc->Get();
+        lenOut = doc.Size();
+        return doc.Get();
     }
 
     virtual ImageData *GetImageData(size_t index) {
@@ -643,7 +647,6 @@ CMobiDoc::CMobiDoc(const TCHAR *fileName) : fileName(str::Dup(fileName)),
 CMobiDoc::~CMobiDoc()
 {
     CloseHandle(fileHandle);
-    free(fileName);
     free(firstRecData);
     free(recHeaders);
     free(bufDynamic);
@@ -652,7 +655,6 @@ CMobiDoc::~CMobiDoc()
         free(images.At(i).id);
     }
     delete huffDic;
-    delete doc;
 }
 
 bool CMobiDoc::ParseHeader()
@@ -1020,13 +1022,12 @@ bool CMobiDoc::LoadDocRecordIntoBuffer(size_t recNo, str::Str<char>& strOut)
 bool CMobiDoc::LoadDocument()
 {
     assert(docUncompressedSize > 0);
-    assert(!doc);
-    doc = new str::Str<char>(docUncompressedSize);
+    doc.EnsureCap(docUncompressedSize);
     for (size_t i = 1; i <= docRecCount; i++) {
-        if (!LoadDocRecordIntoBuffer(i, *doc))
+        if (!LoadDocRecordIntoBuffer(i, doc))
             return false;
     }
-    assert(docUncompressedSize == doc->Size());
+    assert(docUncompressedSize == doc.Size());
     return true;
 }
 

src/Tester.cpp
@@ -159,12 +159,12 @@ static void MobiLayout(const TCHAR *file)
         _tprintf(_T("MobiLayout: file %s doesn't exist or not a mobi file"), file);
         return;
     }
-    _tprintf(_T("Laying out file '%s'\n"), file);
     BaseEbookDoc *doc = ParseEbook(file);
     if (!doc) {
         printf("MobiLayout: failed to parse the file\n");
         return;
     }
+    _tprintf(_T("Laying out file '%s'\n"), doc->GetFilepath());
 
     LayoutInfo li;
     li.pageDx = 640;