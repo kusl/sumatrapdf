kusl/sumatrapdf

commit 2dfa20ff7dd7cb635974a5c964a9e8907bc4970e

merge latest mupdf changes
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5500 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/xps/xps_glyphs.c
@@ -539,7 +539,7 @@ xps_parse_glyphs(xps_document *doc, fz_matrix ctm,
 
 		xps_select_best_font_encoding(doc, font);
 
-		xps_insert_font(doc, partname, font);
+		xps_insert_font(doc, part->name, font);
 
 		/* NOTE: we keep part->data in the font */
 		font->ft_data = part->data;

mupdf/Makefile
@@ -56,19 +56,16 @@ $(OUT)/%.o : xps/%.c fitz/fitz.h xps/muxps.h | $(OUT)
 	$(CC_CMD)
 $(OUT)/%.o : cbz/%.c fitz/fitz.h cbz/mucbz.h | $(OUT)
 	$(CC_CMD)
-$(OUT)/%.o : apps/%.c fitz/fitz.h pdf/mupdf.h xps/muxps.h | $(OUT)
+$(OUT)/%.o : apps/%.c fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h | $(OUT)
 	$(CC_CMD)
 $(OUT)/%.o : scripts/%.c | $(OUT)
 	$(CC_CMD)
 
 .PRECIOUS : $(OUT)/%.o # Keep intermediates from chained rules
 
-# --- Fitz, MuPDF, MuXPS and MuCBZ libraries ---
+# --- Fitz, MuPDF, MuXPS and MuCBZ library ---
 
 FITZ_LIB := $(OUT)/libfitz.a
-MUPDF_LIB := $(OUT)/libmupdf.a
-MUXPS_LIB := $(OUT)/libmuxps.a
-MUCBZ_LIB := $(OUT)/libmucbz.a
 
 FITZ_SRC := $(notdir $(wildcard fitz/*.c draw/*.c))
 FITZ_SRC := $(filter-out draw_simple_scale.c, $(FITZ_SRC))
@@ -77,11 +74,11 @@ MUXPS_SRC := $(notdir $(wildcard xps/*.c))
 MUCBZ_SRC := $(notdir $(wildcard cbz/*.c))
 
 $(FITZ_LIB) : $(addprefix $(OUT)/, $(FITZ_SRC:%.c=%.o))
-$(MUPDF_LIB) : $(addprefix $(OUT)/, $(MUPDF_SRC:%.c=%.o))
-$(MUXPS_LIB) : $(addprefix $(OUT)/, $(MUXPS_SRC:%.c=%.o))
-$(MUCBZ_LIB) : $(addprefix $(OUT)/, $(MUCBZ_SRC:%.c=%.o))
+$(FITZ_LIB) : $(addprefix $(OUT)/, $(MUPDF_SRC:%.c=%.o))
+$(FITZ_LIB) : $(addprefix $(OUT)/, $(MUXPS_SRC:%.c=%.o))
+$(FITZ_LIB) : $(addprefix $(OUT)/, $(MUCBZ_SRC:%.c=%.o))
 
-libs: $(MUCBZ_LIB) $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(THIRD_LIBS)
+libs: $(FITZ_LIB) $(THIRD_LIBS)
 
 # --- Generated CMAP and FONT files ---
 
@@ -130,17 +127,18 @@ $(OUT)/cmapdump.o : pdf/pdf_cmap.c pdf/pdf_cmap_parse.c
 
 PDF_APPS := $(addprefix $(OUT)/, mupdfdraw mupdfclean mupdfextract mupdfinfo mupdfshow)
 XPS_APPS := $(addprefix $(OUT)/, muxpsdraw)
-BUSY_APP := $(addprefix $(OUT)/, mubusy)
+
+$(PDF_APPS) : $(FITZ_LIB) $(THIRD_LIBS)
+$(XPS_APPS) : $(FITZ_LIB) $(THIRD_LIBS)
+
 BUSY_SRC := $(notdir $(wildcard apps/mubusy_*.c))
-BUSY_LIBS := $(addprefix $(OUT)/, $(BUSY_SRC:%.c=%.o))
+BUSY_APP := $(addprefix $(OUT)/, mubusy)
+$(BUSY_APP) : $(addprefix $(OUT)/, $(BUSY_SRC:%.c=%.o))
+$(BUSY_APP) : $(FITZ_LIB) $(THIRD_LIBS)
 
-$(PDF_APPS) : $(MUPDF_LIB) $(FITZ_LIB) $(THIRD_LIBS)
-$(XPS_APPS) : $(MUXPS_LIB) $(FITZ_LIB) $(THIRD_LIBS)
-$(BUSY_APP) : $(BUSY_LIBS) $(MUPDF_LIB) $(MUXPS_LIB) $(FITZ_LIB) $(THIRD_LIBS)
-
-MUPDF := $(OUT)/mupdf
-$(MUPDF) : $(MUCBZ_LIB) $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(THIRD_LIBS)
 ifeq "$(NOX11)" ""
+MUPDF := $(OUT)/mupdf
+$(MUPDF) : $(FITZ_LIB) $(THIRD_LIBS)
 $(MUPDF) : $(addprefix $(OUT)/, x11_main.o x11_image.o pdfapp.o)
 	$(LINK_CMD) $(X11_LIBS)
 endif
@@ -153,11 +151,11 @@ libdir ?= $(prefix)/lib
 incdir ?= $(prefix)/include
 mandir ?= $(prefix)/share/man
 
-install: $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(PDF_APPS) $(XPS_APPS) $(MUPDF)
+install: $(FITZ_LIB) $(PDF_APPS) $(XPS_APPS) $(MUPDF)
 	install -d $(bindir) $(libdir) $(incdir) $(mandir)/man1
-	install $(MUXPS_LIB) $(MUPDF_LIB) $(FITZ_LIB) $(libdir)
-	install fitz/memento.h fitz/fitz.h pdf/mupdf.h xps/muxps.h $(incdir)
-	install cbz/mucbz.h $(PDF_APPS) $(XPS_APPS) $(MUPDF) $(bindir)
+	install $(FITZ_LIB) $(libdir)
+	install fitz/memento.h fitz/fitz.h pdf/mupdf.h xps/muxps.h cbz/mucbz.h $(incdir)
+	install $(PDF_APPS) $(XPS_APPS) $(MUPDF) $(bindir)
 	install $(wildcard apps/man/*.1) $(mandir)/man1
 
 # --- Clean and Default ---

mupdf/android/jni/mupdf.c
@@ -35,7 +35,6 @@ JNIEXPORT int JNICALL
 Java_com_artifex_mupdf_MuPDFCore_openFile(JNIEnv * env, jobject thiz, jstring jfilename)
 {
 	const char *filename;
-	int accelerate = 1;
 	int pages = 0;
 
 	filename = (*env)->GetStringUTFChars(env, jfilename, NULL);
@@ -45,11 +44,8 @@ Java_com_artifex_mupdf_MuPDFCore_openFile(JNIEnv * env, jobject thiz, jstring jf
 		return 0;
 	}
 
-	if (accelerate)
-		fz_accelerate();
-
 	/* 128 MB store for low memory devices. Tweak as necessary. */
-	ctx = fz_new_context(NULL, 128 << 20);
+	ctx = fz_new_context(NULL, NULL, 128 << 20);
 	if (!ctx)
 	{
 		LOGE("Failed to initialise context");

mupdf/apps/mupdfclean.c
@@ -782,7 +782,7 @@ int main(int argc, char **argv)
 	if (argc - fz_optind > 0)
 		subset = 1;
 
-	ctx = fz_new_context(NULL, FZ_STORE_UNLIMITED);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

mupdf/apps/mupdfdraw.c
@@ -49,7 +49,6 @@ static void usage(void)
 #endif
 		"\t-p -\tpassword\n"
 		"\t-r -\tresolution in dpi (default: 72)\n"
-		"\t-A\tdisable accelerated functions\n"
 		"\t-a\tsave alpha channel (only pam and png)\n"
 		"\t-b -\tnumber of bits of antialiasing (0 to 8)\n"
 		"\t-g\trender in grayscale\n"
@@ -475,14 +474,13 @@ int main(int argc, char **argv)
 {
 	char *password = "";
 	int grayscale = 0;
-	int accelerate = 1;
 	pdf_document *doc = NULL;
 	int c;
 	fz_context *ctx;
 
 	fz_var(doc);
 
-	while ((c = fz_getopt(argc, argv, "lo:p:r:R:Aab:dgmtx5G:I")) != -1)
+	while ((c = fz_getopt(argc, argv, "lo:p:r:R:ab:dgmtx5G:I")) != -1)
 	{
 		switch (c)
 		{
@@ -490,7 +488,6 @@ int main(int argc, char **argv)
 		case 'p': password = fz_optarg; break;
 		case 'r': resolution = atof(fz_optarg); break;
 		case 'R': rotation = atof(fz_optarg); break;
-		case 'A': accelerate = 0; break;
 		case 'a': savealpha = 1; break;
 		case 'b': alphabits = atoi(fz_optarg); break;
 		case 'l': showoutline++; break;
@@ -515,10 +512,7 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 
-	if (accelerate)
-		fz_accelerate();
-
-	ctx = fz_new_context(NULL, FZ_STORE_DEFAULT);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

mupdf/apps/mupdfextract.c
@@ -182,7 +182,7 @@ int main(int argc, char **argv)
 
 	infile = argv[fz_optind++];
 
-	ctx = fz_new_context(NULL, FZ_STORE_UNLIMITED);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

mupdf/apps/mupdfinfo.c
@@ -971,7 +971,7 @@ int main(int argc, char **argv)
 	if (fz_optind == argc)
 		infousage();
 
-	ctx = fz_new_context(NULL, FZ_STORE_UNLIMITED);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

mupdf/apps/mupdfshow.c
@@ -198,7 +198,7 @@ int main(int argc, char **argv)
 
 	filename = argv[fz_optind++];
 
-	ctx = fz_new_context(NULL, FZ_STORE_UNLIMITED);
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
 	if (!ctx)
 	{
 		fprintf(stderr, "cannot initialise context\n");

commit 6a0c8efb3739a99b12a589086eb2fdd17c3bf894

revert accidental regression in r5500
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5503 841c7e00-0c1d-0410-a694-8ba26473ccb1

mupdf/xps/xps_glyphs.c
@@ -539,7 +539,8 @@ xps_parse_glyphs(xps_document *doc, fz_matrix ctm,
 
 		xps_select_best_font_encoding(doc, font);
 
-		xps_insert_font(doc, part->name, font);
+		/* SumatraPDF: support StyleSimulations */
+		xps_insert_font(doc, partname, font);
 
 		/* NOTE: we keep part->data in the font */
 		font->ft_data = part->data;