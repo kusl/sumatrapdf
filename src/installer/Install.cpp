kusl/sumatrapdf

commit 258ebbe0f15fd49aaf7b7d06a20c195a11f4d4fe

use file::Exists() instead of _taccess (which is not implemented in ucrt
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5609 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/installer/Install.cpp
@@ -495,8 +495,8 @@ static int CALLBACK BrowseCallbackProc(HWND hwnd, UINT msg, LPARAM lParam, LPARA
     // disable the OK button for non-filesystem and inaccessible folders (and shortcuts to folders)
     case BFFM_SELCHANGED:
         {
-            TCHAR szDir[MAX_PATH];
-            if (SHGetPathFromIDList((LPITEMIDLIST)lParam, szDir) && _taccess(szDir, 00) == 0) {
+            TCHAR path[MAX_PATH];
+            if (SHGetPathFromIDList((LPITEMIDLIST)lParam, path) && file::Exists(path)) {
                 SHFILEINFO sfi;
                 SHGetFileInfo((LPCTSTR)lParam, 0, &sfi, sizeof(sfi), SHGFI_PIDL | SHGFI_ATTRIBUTES);
                 if (!(sfi.dwAttributes & SFGAO_LINK))

commit 3b81ac0294a8e257d4ecb9eab512a78171e0c2f6

fix installer regression from r5609
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@5901 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/installer/Install.cpp
@@ -496,7 +496,7 @@ static int CALLBACK BrowseCallbackProc(HWND hwnd, UINT msg, LPARAM lParam, LPARA
     case BFFM_SELCHANGED:
         {
             TCHAR path[MAX_PATH];
-            if (SHGetPathFromIDList((LPITEMIDLIST)lParam, path) && file::Exists(path)) {
+            if (SHGetPathFromIDList((LPITEMIDLIST)lParam, path) && dir::Exists(path)) {
                 SHFILEINFO sfi;
                 SHGetFileInfo((LPCTSTR)lParam, 0, &sfi, sizeof(sfi), SHGFI_PIDL | SHGFI_ATTRIBUTES);
                 if (!(sfi.dwAttributes & SFGAO_LINK))