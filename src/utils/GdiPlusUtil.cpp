kusl/sumatrapdf

commit b251680eaf2bc69840df51e6ef808086298310cb

tweak MeasureTextQuick
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@6301 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/utils/GdiPlusUtil.cpp
@@ -83,8 +83,12 @@ RectF MeasureTextQuick(Graphics *g, Font *f, const WCHAR *s, size_t len)
 {
     RectF bbox;
     g->MeasureString(s, len, f, PointF(0, 0), &bbox);
+    INT style = f->GetStyle();
+    LOGFONTW lfw;
+    Status ok = f->GetLogFontW(g, &lfw);
     // most documents look good enough with these adjustments
-    bbox.Width *= 0.92f;
+    if (!(style & FontStyleItalic) && Ok == ok && !str::Eq(lfw.lfFaceName, L"Courier New"))
+        bbox.Width *= 0.92f;
     bbox.Height *= 0.96f;
     return bbox;
 }

scripts/generate-htmlparserlookup.py
@@ -119,7 +119,7 @@ def createFastSelector(fullList, nameList, funcName, type):
 
 # This list has been generated by instrumenting HtmlFormatter.cpp
 # to dump all tags we see in a mobi file and also some from EPUB and FB2 files
-List_HTML_Tags = "a abbr acronym area audio b base basefont blockquote body br center code col dd div dl dt em font frame guide h1 h2 h3 h4 h5 head hr html i img image input lh li link mbp:pagebreak meta object ol p pagebreak param pre reference s section small span strike strong style sub subtitle sup table td th title tr tt u ul video"
+List_HTML_Tags = "a abbr acronym area audio b base basefont blockquote body br center code col dd div dl dt em font frame guide h1 h2 h3 h4 h5 h6 head hr html i img image input lh li link mbp:pagebreak meta object ol p pagebreak param pre reference s section small span strike strong style sub subtitle sup table td th title tr tt u ul video"
 List_HTML_Attrs = "size href color filepos border valign rowspan colspan link vlink style face value bgcolor class id mediarecindex controls recindex title lang clear xmlns xmlns:dc width align height"
 List_Align_Attrs = "left right center justify"
 

src/HtmlFormatter.cpp
@@ -770,7 +770,7 @@ bool HtmlFormatter::HandleTagA(HtmlToken *t, const char *linkAttr)
     return false;
 }
 
-static bool IsTagH(HtmlTag tag)
+inline bool IsTagH(HtmlTag tag)
 {
     switch (tag) {
         case Tag_H1:
@@ -778,6 +778,7 @@ static bool IsTagH(HtmlTag tag)
         case Tag_H3:
         case Tag_H4:
         case Tag_H5:
+        case Tag_H6:
             return true;
     }
    return false;

src/utils/HtmlParserLookup.cpp
@@ -77,6 +77,7 @@ HtmlTag FindHtmlTag(const char *name, size_t len)
     case CS2('h','3'): return Tag_H3;
     case CS2('h','4'): return Tag_H4;
     case CS2('h','5'): return Tag_H5;
+    case CS2('h','6'): return Tag_H6;
     case CS4('h','e','a','d'):
         if (4 == len) return Tag_Head;
         break;

src/utils/HtmlParserLookup.h
@@ -12,15 +12,15 @@ enum HtmlTag {
     Tag_Br, Tag_Center, Tag_Code, Tag_Col, Tag_Dd,
     Tag_Div, Tag_Dl, Tag_Dt, Tag_Em, Tag_Font,
     Tag_Frame, Tag_Guide, Tag_H1, Tag_H2, Tag_H3,
-    Tag_H4, Tag_H5, Tag_Head, Tag_Hr, Tag_Html,
-    Tag_I, Tag_Image, Tag_Img, Tag_Input, Tag_Lh,
-    Tag_Li, Tag_Link, Tag_Mbp_Pagebreak, Tag_Meta, Tag_Object,
-    Tag_Ol, Tag_P, Tag_Pagebreak, Tag_Param, Tag_Pre,
-    Tag_Reference, Tag_S, Tag_Section, Tag_Small, Tag_Span,
-    Tag_Strike, Tag_Strong, Tag_Style, Tag_Sub, Tag_Subtitle,
-    Tag_Sup, Tag_Table, Tag_Td, Tag_Th, Tag_Title,
-    Tag_Tr, Tag_Tt, Tag_U, Tag_Ul, Tag_Video,
-    Tag_NotFound
+    Tag_H4, Tag_H5, Tag_H6, Tag_Head, Tag_Hr,
+    Tag_Html, Tag_I, Tag_Image, Tag_Img, Tag_Input,
+    Tag_Lh, Tag_Li, Tag_Link, Tag_Mbp_Pagebreak, Tag_Meta,
+    Tag_Object, Tag_Ol, Tag_P, Tag_Pagebreak, Tag_Param,
+    Tag_Pre, Tag_Reference, Tag_S, Tag_Section, Tag_Small,
+    Tag_Span, Tag_Strike, Tag_Strong, Tag_Style, Tag_Sub,
+    Tag_Subtitle, Tag_Sup, Tag_Table, Tag_Td, Tag_Th,
+    Tag_Title, Tag_Tr, Tag_Tt, Tag_U, Tag_Ul,
+    Tag_Video, Tag_NotFound
 };
 
 enum AlignAttr {

commit 24123048e11353dba5c46f1aa59357d4753f5943

fix perf regression in r6301
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@6302 841c7e00-0c1d-0410-a694-8ba26473ccb1

src/utils/GdiPlusUtil.cpp
@@ -81,13 +81,24 @@ RectF MeasureTextStandard(Graphics *g, Font *f, const WCHAR *s, size_t len)
 
 RectF MeasureTextQuick(Graphics *g, Font *f, const WCHAR *s, size_t len)
 {
+    static Vec<Font *> fontCache;
+    static Vec<bool> fixCache;
+
     RectF bbox;
     g->MeasureString(s, len, f, PointF(0, 0), &bbox);
-    INT style = f->GetStyle();
-    LOGFONTW lfw;
-    Status ok = f->GetLogFontW(g, &lfw);
+    int idx = fontCache.Find(f);
+    if (-1 == idx) {
+        INT style = f->GetStyle();
+        LOGFONTW lfw;
+        Status ok = f->GetLogFontW(g, &lfw);
+        bool isItalicOrMonospace = (style & FontStyleItalic) || Ok != ok ||
+                                   str::Eq(lfw.lfFaceName, L"Courier New");
+        fontCache.Append(f);
+        fixCache.Append(isItalicOrMonospace);
+        idx = (int)fontCache.Count() - 1;
+    }
     // most documents look good enough with these adjustments
-    if (!(style & FontStyleItalic) && Ok == ok && !str::Eq(lfw.lfFaceName, L"Courier New"))
+    if (!fixCache.At(idx))
         bbox.Width *= 0.92f;
     bbox.Height *= 0.96f;
     return bbox;