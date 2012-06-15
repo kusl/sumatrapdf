kusl/sumatrapdf

commit a41e56745dc9a907a779ee465d71f61b52570cb0

merge latest jbig2dec update
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@6504 841c7e00-0c1d-0410-a694-8ba26473ccb1

ext/_patches/jbig2dec.patch
@@ -1,161 +1,6 @@
-diff -rPu5 jbig2dec.orig\getopt.c jbig2dec\getopt.c
---- jbig2dec.orig\getopt.c	Fri Jun 15 13:36:58 2012
-+++ jbig2dec\getopt.c	Fri Jun 15 14:05:54 2012
-@@ -1,25 +1,28 @@
--/* Copyright (C) 2001-2012 Artifex Software, Inc.
--   All Rights Reserved.
--
--   This software is provided AS-IS with no warranty, either express or
--   implied.
--
--   This software is distributed under license and may not be copied,
--   modified or distributed except as expressly authorized under the terms
--   of the license contained in the file LICENSE in this distribution.
--
--   Refer to licensing information at http://www.artifex.com or contact
--   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
--   CA  94903, U.S.A., +1(415)492-9861, for further information.
--*/
--
- /* Getopt for GNU.
-    NOTE: getopt is now part of the C library, so if you don't know what
-    "Keep this file name-space clean" means, talk to drepper@gnu.org
-    before changing it!
--*/
-+   Copyright (C) 1987,88,89,90,91,92,93,94,95,96,98,99,2000,2001
-+   	Free Software Foundation, Inc.
-+   This file is part of the GNU C Library.
-+
-+   The GNU C Library is free software; you can redistribute it and/or
-+   modify it under the terms of the GNU Lesser General Public
-+   License as published by the Free Software Foundation; either
-+   version 2.1 of the License, or (at your option) any later version.
-+
-+   The GNU C Library is distributed in the hope that it will be useful,
-+   but WITHOUT ANY WARRANTY; without even the implied warranty of
-+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-+   Lesser General Public License for more details.
-+
-+   You should have received a copy of the GNU Lesser General Public
-+   License along with the GNU C Library; if not, write to the Free
-+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-+   02111-1307 USA.  */
-+
- /* This tells Alpha OSF/1 not to define a getopt prototype in <stdio.h>.
-    Ditto for AIX 3.2 and <stdlib.h>.  */
- #ifndef _NO_PROTO
- # define _NO_PROTO
- #endif
-diff -rPu5 jbig2dec.orig\getopt.h jbig2dec\getopt.h
---- jbig2dec.orig\getopt.h	Fri Jun 15 13:36:58 2012
-+++ jbig2dec\getopt.h	Fri Jun 15 14:05:54 2012
-@@ -1,22 +1,23 @@
--/* Copyright (C) 2001-2012 Artifex Software, Inc.
--   All Rights Reserved.
--
--   This software is provided AS-IS with no warranty, either express or
--   implied.
-+/* Declarations for getopt.
-+   Copyright (C) 1989-1994, 1996-1999, 2001 Free Software Foundation, Inc.
-+   This file is part of the GNU C Library.
- 
--   This software is distributed under license and may not be copied,
--   modified or distributed except as expressly authorized under the terms
--   of the license contained in the file LICENSE in this distribution.
-+   The GNU C Library is free software; you can redistribute it and/or
-+   modify it under the terms of the GNU Lesser General Public
-+   License as published by the Free Software Foundation; either
-+   version 2.1 of the License, or (at your option) any later version.
- 
--   Refer to licensing information at http://www.artifex.com or contact
--   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
--   CA  94903, U.S.A., +1(415)492-9861, for further information.
--*/
-+   The GNU C Library is distributed in the hope that it will be useful,
-+   but WITHOUT ANY WARRANTY; without even the implied warranty of
-+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-+   Lesser General Public License for more details.
- 
--/* Declarations for getopt.
--*/
-+   You should have received a copy of the GNU Lesser General Public
-+   License along with the GNU C Library; if not, write to the Free
-+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-+   02111-1307 USA.  */
- 
- #ifndef _GETOPT_H
- 
- #ifndef __need_getopt
- # define _GETOPT_H 1
-diff -rPu5 jbig2dec.orig\getopt1.c jbig2dec\getopt1.c
---- jbig2dec.orig\getopt1.c	Fri Jun 15 13:36:58 2012
-+++ jbig2dec\getopt1.c	Fri Jun 15 14:05:54 2012
-@@ -1,22 +1,25 @@
--/* Copyright (C) 2001-2012 Artifex Software, Inc.
--   All Rights Reserved.
--
--   This software is provided AS-IS with no warranty, either express or
--   implied.
-+/* getopt_long and getopt_long_only entry points for GNU getopt.
-+   Copyright (C) 1987,88,89,90,91,92,93,94,96,97,98
-+     Free Software Foundation, Inc.
-+   This file is part of the GNU C Library.
- 
--   This software is distributed under license and may not be copied,
--   modified or distributed except as expressly authorized under the terms
--   of the license contained in the file LICENSE in this distribution.
-+   The GNU C Library is free software; you can redistribute it and/or
-+   modify it under the terms of the GNU Lesser General Public
-+   License as published by the Free Software Foundation; either
-+   version 2.1 of the License, or (at your option) any later version.
- 
--   Refer to licensing information at http://www.artifex.com or contact
--   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
--   CA  94903, U.S.A., +1(415)492-9861, for further information.
--*/
-+   The GNU C Library is distributed in the hope that it will be useful,
-+   but WITHOUT ANY WARRANTY; without even the implied warranty of
-+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-+   Lesser General Public License for more details.
- 
--/* getopt_long and getopt_long_only entry points for GNU getopt.
--*/
-+   You should have received a copy of the GNU Lesser General Public
-+   License along with the GNU C Library; if not, write to the Free
-+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-+   02111-1307 USA.  */
-+
- #ifdef HAVE_CONFIG_H
- #include <config.h>
- #endif
- 
- #include "getopt.h"
-diff -rPu5 jbig2dec.orig\jbig2.h jbig2dec\jbig2.h
---- jbig2dec.orig\jbig2.h	Fri Jun 15 13:36:58 2012
-+++ jbig2dec\jbig2.h	Fri Jun 15 14:09:17 2012
-@@ -27,16 +27,19 @@
- 
- /* To enable Memento, either uncomment the following, or arrange to
-  * predefine MEMENTO whilst building. */
- /* #define MEMENTO */
- 
-+/* SumatraPDF: allow to build without Memento */
-+#ifdef MEMENTO
- /* If we are building as part of GS then make sure we use the version
-  * of MEMENTO that is part of gs (in case of version skew) */
- #ifdef GSBUILD
- #include "../base/memento.h"
- #else
- #include "memento.h"
-+#endif
- #endif
- 
- /* warning levels */
- typedef enum {
-   JBIG2_SEVERITY_DEBUG,
 diff -rPu5 jbig2dec.orig\jbig2_image.c jbig2dec\jbig2_image.c
 --- jbig2dec.orig\jbig2_image.c	Fri Jun 15 13:36:58 2012
-+++ jbig2dec\jbig2_image.c	Fri Jun 15 14:09:34 2012
++++ jbig2dec\jbig2_image.c	Fri Jun 15 14:16:17 2012
 @@ -46,11 +46,12 @@
  		return NULL;
  	}
@@ -170,3 +15,19 @@ diff -rPu5 jbig2dec.orig\jbig2_image.c jbig2dec\jbig2_image.c
              jbig2_error(ctx, JBIG2_SEVERITY_FATAL, -1,
                  "integer multiplication overflow from stride(%d)*height(%d)",
                  stride, height);
+diff -rPu5 jbig2dec.orig\jbig2_symbol_dict.c jbig2dec\jbig2_symbol_dict.c
+--- jbig2dec.orig\jbig2_symbol_dict.c	Fri Jun 15 20:45:09 2012
++++ jbig2dec\jbig2_symbol_dict.c	Fri Jun 15 20:50:31 2012
+@@ -289,11 +289,11 @@
+               "failed to allocate storage for symbol bitmap");
+           goto cleanup1;
+       }
+       if (params->SDREFAGG) {
+           int64_t tmp = params->SDNUMINSYMS + params->SDNUMNEWSYMS;
+-          for (SBSYMCODELEN = 0; (1 << SBSYMCODELEN) < tmp; SBSYMCODELEN++);
++          for (SBSYMCODELEN = 0; (int64_t)(1 << SBSYMCODELEN) < tmp; SBSYMCODELEN++);
+           IAID = jbig2_arith_iaid_ctx_new(ctx, SBSYMCODELEN);
+           IARDX = jbig2_arith_int_ctx_new(ctx);
+           IARDY = jbig2_arith_int_ctx_new(ctx);
+           if ((IAID == NULL) || (IARDX == NULL) || (IARDY == NULL))
+           {

ext/jbig2dec/jbig2.h
@@ -29,15 +29,12 @@ extern "C" {
  * predefine MEMENTO whilst building. */
 /* #define MEMENTO */
 
-/* SumatraPDF: allow to build without Memento */
-#ifdef MEMENTO
 /* If we are building as part of GS then make sure we use the version
  * of MEMENTO that is part of gs (in case of version skew) */
 #ifdef GSBUILD
 #include "../base/memento.h"
 #else
 #include "memento.h"
-#endif
 #endif
 
 /* warning levels */

ext/jbig2dec/jbig2.c
@@ -57,9 +57,14 @@ static Jbig2Allocator jbig2_default_allocator =
 };
 
 void *
-jbig2_alloc (Jbig2Allocator *allocator, size_t size)
+jbig2_alloc (Jbig2Allocator *allocator, size_t size, size_t num)
 {
-  return allocator->alloc (allocator, size);
+  /* check for integer multiplication overflow */
+  int64_t check = ((int64_t)num)*((int64_t)size);
+  if (check != (int)check)
+    return NULL;
+  else
+    return allocator->alloc (allocator, (int)check);
 }
 
 void
@@ -69,9 +74,14 @@ jbig2_free (Jbig2Allocator *allocator, void *p)
 }
 
 void *
-jbig2_realloc (Jbig2Allocator *allocator, void *p, size_t size)
+jbig2_realloc (Jbig2Allocator *allocator, void *p, size_t size, size_t num)
 {
-  return allocator->realloc (allocator, p, size);
+  /* check for integer multiplication overflow */
+  int64_t check = ((int64_t)num)*((int64_t)size);
+  if (check != (int)check)
+    return NULL;
+  else
+    return allocator->realloc (allocator, p, (int)check);
 }
 
 static int
@@ -123,7 +133,7 @@ jbig2_ctx_new (Jbig2Allocator *allocator,
   if (error_callback == NULL)
       error_callback = &jbig2_default_error;
 
-  result = (Jbig2Ctx*)jbig2_alloc(allocator, sizeof(Jbig2Ctx));
+  result = (Jbig2Ctx*)jbig2_alloc(allocator, sizeof(Jbig2Ctx), 1);
   if (result == NULL) {
     error_callback(error_callback_data, "initial context allocation failed!",
                     JBIG2_SEVERITY_FATAL, -1);

ext/jbig2dec/jbig2_priv.h
@@ -92,17 +92,17 @@ jbig2_get_int16 (const byte *buf);
 
 /* dynamic memory management */
 void *
-jbig2_alloc (Jbig2Allocator *allocator, size_t size);
+jbig2_alloc (Jbig2Allocator *allocator, size_t size, size_t num);
 
 void
 jbig2_free (Jbig2Allocator *allocator, void *p);
 
 void *
-jbig2_realloc (Jbig2Allocator *allocator, void *p, size_t size);
+jbig2_realloc (Jbig2Allocator *allocator, void *p, size_t size, size_t num);
 
-#define jbig2_new(ctx, t, size) ((t *)jbig2_alloc(ctx->allocator, (size) * sizeof(t)))
+#define jbig2_new(ctx, t, size) ((t *)jbig2_alloc(ctx->allocator, size, sizeof(t)))
 
-#define jbig2_renew(ctx, p, t, size) ((t *)jbig2_realloc(ctx->allocator, (p), (size) * sizeof(t)))
+#define jbig2_renew(ctx, p, t, size) ((t *)jbig2_realloc(ctx->allocator, (p), size, sizeof(t)))
 
 int
 jbig2_error (Jbig2Ctx *ctx, Jbig2Severity severity, int32_t seg_idx,

ext/jbig2dec/jbig2_symbol_dict.c
@@ -48,7 +48,7 @@
 typedef struct {
   bool SDHUFF;
   bool SDREFAGG;
-  int32_t SDNUMINSYMS;
+  uint32_t SDNUMINSYMS;
   Jbig2SymbolDict *SDINSYMS;
   uint32_t SDNUMNEWSYMS;
   uint32_t SDNUMEXSYMS;
@@ -233,7 +233,7 @@ jbig2_decode_symbol_dict(Jbig2Ctx *ctx,
 {
   Jbig2SymbolDict *SDNEWSYMS = NULL;
   Jbig2SymbolDict *SDEXSYMS = NULL;
-  int32_t HCHEIGHT;
+  uint32_t HCHEIGHT;
   uint32_t NSYMSDECODED;
   uint32_t SYMWIDTH, TOTWIDTH;
   uint32_t HCFIRSTSYM;
@@ -290,8 +290,8 @@ jbig2_decode_symbol_dict(Jbig2Ctx *ctx,
           goto cleanup1;
       }
       if (params->SDREFAGG) {
-          int tmp = params->SDNUMINSYMS + params->SDNUMNEWSYMS;
-          for (SBSYMCODELEN = 0; (1 << SBSYMCODELEN) < tmp; SBSYMCODELEN++);
+          int64_t tmp = params->SDNUMINSYMS + params->SDNUMNEWSYMS;
+          for (SBSYMCODELEN = 0; (int64_t)(1 << SBSYMCODELEN) < tmp; SBSYMCODELEN++);
           IAID = jbig2_arith_iaid_ctx_new(ctx, SBSYMCODELEN);
           IARDX = jbig2_arith_int_ctx_new(ctx);
           IARDY = jbig2_arith_int_ctx_new(ctx);
@@ -318,7 +318,8 @@ jbig2_decode_symbol_dict(Jbig2Ctx *ctx,
 	  SDNEWSYMWIDTHS = jbig2_new(ctx, uint32_t, params->SDNUMNEWSYMS);
 	  if (SDNEWSYMWIDTHS == NULL) {
 	    jbig2_error(ctx, JBIG2_SEVERITY_FATAL, segment->number,
-            "could not allocate storage for symbol widths");
+              "could not allocate storage for (%u) symbol widths",
+              params->SDNUMNEWSYMS);
 	    goto cleanup2;
 	  }
       }
@@ -327,7 +328,8 @@ jbig2_decode_symbol_dict(Jbig2Ctx *ctx,
   SDNEWSYMS = jbig2_sd_new(ctx, params->SDNUMNEWSYMS);
   if (SDNEWSYMS == NULL) {
       jbig2_error(ctx, JBIG2_SEVERITY_WARNING, segment->number,
-          "could not allocate storage for symbols");
+          "could not allocate storage for (%u) new symbols",
+          params->SDNUMNEWSYMS);
       goto cleanup2;
   }
 
@@ -634,7 +636,7 @@ jbig2_decode_symbol_dict(Jbig2Ctx *ctx,
 	  }
 
 	  jbig2_error(ctx, JBIG2_SEVERITY_DEBUG, segment->number,
-            "decoded symbol %d of %d (%dx%d)",
+            "decoded symbol %u of %u (%ux%u)",
 		NSYMSDECODED, params->SDNUMNEWSYMS,
 		SYMWIDTH, HCHEIGHT);
 
@@ -1002,7 +1004,7 @@ jbig2_symbol_dictionary(Jbig2Ctx *ctx, Jbig2Segment *segment,
   offset += 8;
 
   jbig2_error(ctx, JBIG2_SEVERITY_INFO, segment->number,
-	      "symbol dictionary, flags=%04x, %d exported syms, %d new syms",
+	      "symbol dictionary, flags=%04x, %u exported syms, %u new syms",
 	      flags, params.SDNUMEXSYMS, params.SDNUMNEWSYMS);
 
   /* 7.4.2.2 (2) */

ext/jbig2dec/jbig2_symbol_dict.h
@@ -22,7 +22,7 @@
 
 /* the results of decoding a symbol dictionary */
 typedef struct {
-    int n_symbols;
+    uint32_t n_symbols;
     Jbig2Image **glyphs;
 } Jbig2SymbolDict;
 

ext/jbig2dec/memento.h
@@ -1,0 +1,230 @@
+/* Copyright (C) 2001-2012 Artifex Software, Inc.
+   All Rights Reserved.
+
+   This software is provided AS-IS with no warranty, either express or
+   implied.
+
+   This software is distributed under license and may not be copied,
+   modified or distributed except as expressly authorized under the terms
+   of the license contained in the file LICENSE in this distribution.
+
+   Refer to licensing information at http://www.artifex.com or contact
+   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
+   CA  94903, U.S.A., +1(415)492-9861, for further information.
+*/
+
+
+/* Memento: A library to aid debugging of memory leaks/heap corruption.
+ *
+ * Usage:
+ *    First, build your project with MEMENTO defined, and include this
+ *    header file wherever you use malloc, realloc or free.
+ *    This header file will use macros to point malloc, realloc and free to
+ *    point to Memento_malloc, Memento_realloc, Memento_free.
+ *
+ *    Run your program, and all mallocs/frees/reallocs should be redirected
+ *    through here. When the program exits, you will get a list of all the
+ *    leaked blocks, together with some helpful statistics. You can get the
+ *    same list of allocated blocks at any point during program execution by
+ *    calling Memento_listBlocks();
+ *
+ *    Every call to malloc/free/realloc counts as an 'allocation event'.
+ *    On each event Memento increments a counter. Every block is tagged with
+ *    the current counter on allocation. Every so often during program
+ *    execution, the heap is checked for consistency. By default this happens
+ *    every 1024 events. This can be changed at runtime by using
+ *    Memento_setParanoia(int level). 0 turns off such checking, 1 sets
+ *    checking to happen on every event, any other number n sets checking to
+ *    happen once every n events.
+ *
+ *    Memento keeps blocks around for a while after they have been freed, and
+ *    checks them as part of these heap checks to see if they have been
+ *    written to (or are freed twice etc).
+ *
+ *    A given heap block can be checked for consistency (it's 'pre' and
+ *    'post' guard blocks are checked to see if they have been written to)
+ *    by calling Memento_checkBlock(void *blockAddress);
+ *
+ *    A check of all the memory can be triggered by calling Memento_check();
+ *    (or Memento_checkAllMemory(); if you'd like it to be quieter).
+ *
+ *    A good place to breakpoint is Memento_breakpoint, as this will then
+ *    trigger your debugger if an error is detected. This is done
+ *    automatically for debug windows builds.
+ *
+ *    If a block is found to be corrupt, information will be printed to the
+ *    console, including the address of the block, the size of the block,
+ *    the type of corruption, the number of the block and the event on which
+ *    it last passed a check for correctness.
+ *
+ *    If you rerun, and call Memento_paranoidAt(int event); with this number
+ *    the the code will wait until it reaches that event and then start
+ *    checking the heap after every allocation event. Assuming it is a
+ *    deterministic failure, you should then find out where in your program
+ *    the error is occurring (between event x-1 and event x).
+ *
+ *    Then you can rerun the program again, and call
+ *    Memento_breakAt(int event); and the program will call
+ *    Memento_Breakpoint() when event x is reached, enabling you to step
+ *    through.
+ *
+ *    Memento_find(address) will tell you what block (if any) the given
+ *    address is in.
+ *
+ * An example:
+ *    Suppose we have a gs invocation that crashes with memory corruption.
+ *     * Build with -DMEMENTO.
+ *     * In your debugger put breakpoints on Memento_inited and
+ *       Memento_Breakpoint.
+ *     * Run the program. It will stop in Memento_inited.
+ *     * Execute Memento_setParanoia(1);  (In VS use Ctrl-Alt-Q). (Note #1)
+ *     * Continue execution.
+ *     * It will detect the memory corruption on the next allocation event
+ *       after it happens, and stop in Memento_breakpoint. The console should
+ *       show something like:
+ *
+ *       Freed blocks:
+ *         0x172e610(size=288,num=1415) index 256 (0x172e710) onwards corrupted
+ *           Block last checked OK at allocation 1457. Now 1458.
+ *
+ *     * This means that the block became corrupted between allocation 1457
+ *       and 1458 - so if we rerun and stop the program at 1457, we can then
+ *       step through, possibly with a data breakpoint at 0x172e710 and see
+ *       when it occurs.
+ *     * So restart the program from the beginning. When we hit Memento_inited
+ *       execute Memento_breakAt(1457); (and maybe Memento_setParanoia(1), or
+ *       Memento_setParanoidAt(1457))
+ *     * Continue execution until we hit Memento_breakpoint.
+ *     * Now you can step through and watch the memory corruption happen.
+ *
+ *    Note #1: Using Memento_setParanoia(1) can cause your program to run
+ *    very slowly. You may instead choose to use Memento_setParanoia(100)
+ *    (or some other figure). This will only exhaustively check memory on
+ *    every 100th allocation event. This trades speed for the size of the
+ *    average allocation event range in which detection of memory corruption
+ *    occurs. You may (for example) choose to run once checking every 100
+ *    allocations and discover that the corruption happens between events
+ *    X and X+100. You can then rerun using Memento_paranoidAt(X), and
+ *    it'll only start exhaustively checking when it reaches X.
+ *
+ * More than one memory allocator?
+ *
+ *    If you have more than one memory allocator in the system (like for
+ *    instance the ghostscript chunk allocator, that builds on top of the
+ *    standard malloc and returns chunks itself), then there are some things
+ *    to note:
+ *
+ *    * If the secondary allocator gets its underlying blocks from calling
+ *      malloc, then those will be checked by Memento, but 'subblocks' that
+ *      are returned to the secondary allocator will not. There is currently
+ *      no way to fix this other than trying to bypass the secondary
+ *      allocator. One way I have found to do this with the chunk allocator
+ *      is to tweak its idea of a 'large block' so that it puts every
+ *      allocation in its own chunk. Clearly this negates the point of having
+ *      a secondary allocator, and is therefore not recommended for general
+ *      use.
+ *
+ *    * Again, if the secondary allocator gets its underlying blocks from
+ *      calling malloc (and hence Memento) leak detection should still work
+ *      (but whole blocks will be detected rather than subblocks).
+ *
+ *    * If on every allocation attempt the secondary allocator calls into
+ *      Memento_failThisEvent(), and fails the allocation if it returns true
+ *      then more useful features can be used; firstly memory squeezing will
+ *      work, and secondly, Memento will have a "finer grained" paranoia
+ *      available to it.
+ */
+
+#ifndef MEMENTO_H
+
+#include <memory.h>
+
+#define MEMENTO_H
+
+#ifndef MEMENTO_UNDERLYING_MALLOC
+#define MEMENTO_UNDERLYING_MALLOC malloc
+#endif
+#ifndef MEMENTO_UNDERLYING_FREE
+#define MEMENTO_UNDERLYING_FREE free
+#endif
+#ifndef MEMENTO_UNDERLYING_REALLOC
+#define MEMENTO_UNDERLYING_REALLOC realloc
+#endif
+#ifndef MEMENTO_UNDERLYING_CALLOC
+#define MEMENTO_UNDERLYING_CALLOC calloc
+#endif
+
+#ifndef MEMENTO_MAXALIGN
+#define MEMENTO_MAXALIGN (sizeof(int))
+#endif
+
+#define MEMENTO_PREFILL   0xa6
+#define MEMENTO_POSTFILL  0xa7
+#define MEMENTO_ALLOCFILL 0xa8
+#define MEMENTO_FREEFILL  0xa9
+
+#define MEMENTO_FREELIST_MAX 0x2000000
+
+int Memento_checkBlock(void *);
+int Memento_checkAllMemory(void);
+int Memento_check(void);
+
+int Memento_setParanoia(int);
+int Memento_paranoidAt(int);
+int Memento_breakAt(int);
+void Memento_breakOnFree(void *a);
+void Memento_breakOnRealloc(void *a);
+int Memento_getBlockNum(void *);
+int Memento_find(void *a);
+void Memento_breakpoint(void);
+int Memento_failAt(int);
+int Memento_failThisEvent(void);
+void Memento_listBlocks(void);
+void Memento_listNewBlocks(void);
+size_t Memento_setMax(size_t);
+void Memento_stats(void);
+void *Memento_label(void *, const char *);
+
+void *Memento_malloc(size_t s);
+void *Memento_realloc(void *, size_t s);
+void  Memento_free(void *);
+void *Memento_calloc(size_t, size_t);
+
+#ifdef MEMENTO
+
+#ifndef COMPILING_MEMENTO_C
+#define malloc  Memento_malloc
+#define free    Memento_free
+#define realloc Memento_realloc
+#define calloc  Memento_calloc
+#endif
+
+#else
+
+#define Memento_malloc  MEMENTO_UNDERLYING_MALLOC
+#define Memento_free    MEMENTO_UNDERLYING_FREE
+#define Memento_realloc MEMENTO_UNDERLYING_REALLOC
+#define Memento_calloc  MEMENTO_UNDERLYING_CALLOC
+
+#define Memento_checkBlock(A)     0
+#define Memento_checkAllMemory()  0
+#define Memento_check()           0
+#define Memento_setParanoia(A)    0
+#define Memento_paranoidAt(A)     0
+#define Memento_breakAt(A)        0
+#define Memento_breakOnFree(A)    0
+#define Memento_breakOnRealloc(A) 0
+#define Memento_getBlockNum(A)    0
+#define Memento_find(A)           0
+#define Memento_breakpoint()      do {} while (0)
+#define Memento_failAt(A)         0
+#define Memento_failThisEvent()   0
+#define Memento_listBlocks()      do {} while (0)
+#define Memento_listNewBlocks()   do {} while (0)
+#define Memento_setMax(A)         0
+#define Memento_stats()           do {} while (0)
+#define Memento_label(A,B)        (A)
+
+#endif /* MEMENTO */
+
+#endif /* MEMENTO_H */

commit 2c5173f52f4d3f41796dad611779e869119e648a

fix a potential heap overflow and a build regression from r6504
git-svn-id: http://sumatrapdf.googlecode.com/svn/trunk@6505 841c7e00-0c1d-0410-a694-8ba26473ccb1

ext/_patches/jbig2dec.patch
@@ -1,3 +1,26 @@
+diff -rPu5 jbig2dec.orig\jbig2.h jbig2dec\jbig2.h
+--- jbig2dec.orig\jbig2.h	Fri Jun 15 13:36:58 2012
++++ jbig2dec\jbig2.h	Fri Jun 15 23:28:23 2012
+@@ -27,16 +27,19 @@
+ 
+ /* To enable Memento, either uncomment the following, or arrange to
+  * predefine MEMENTO whilst building. */
+ /* #define MEMENTO */
+ 
++/* SumatraPDF: allow to build without MEMENTO (clashes with MuPDF's) */
++#ifdef MEMENTO
+ /* If we are building as part of GS then make sure we use the version
+  * of MEMENTO that is part of gs (in case of version skew) */
+ #ifdef GSBUILD
+ #include "../base/memento.h"
+ #else
+ #include "memento.h"
++#endif
+ #endif
+ 
+ /* warning levels */
+ typedef enum {
+   JBIG2_SEVERITY_DEBUG,
 diff -rPu5 jbig2dec.orig\jbig2_image.c jbig2dec\jbig2_image.c
 --- jbig2dec.orig\jbig2_image.c	Fri Jun 15 13:36:58 2012
 +++ jbig2dec\jbig2_image.c	Fri Jun 15 14:16:17 2012

ext/jbig2dec/jbig2.h
@@ -29,12 +29,15 @@ extern "C" {
  * predefine MEMENTO whilst building. */
 /* #define MEMENTO */
 
+/* SumatraPDF: allow to build without MEMENTO (clashes with MuPDF's) */
+#ifdef MEMENTO
 /* If we are building as part of GS then make sure we use the version
  * of MEMENTO that is part of gs (in case of version skew) */
 #ifdef GSBUILD
 #include "../base/memento.h"
 #else
 #include "memento.h"
+#endif
 #endif
 
 /* warning levels */

mupdf/pdf/pdf_function.c
@@ -1396,7 +1396,8 @@ pdf_load_function(pdf_document *xref, pdf_obj *dict)
 	/* required for all */
 	obj = pdf_dict_gets(dict, "Domain");
 	func->m = pdf_array_len(obj) / 2;
-	for (i = 0; i < func->m; i++)
+	/* SumatraPDF: prevent heap overflow */
+	for (i = 0; i < MIN(func->m, MAXM); i++)
 	{
 		func->domain[i][0] = pdf_to_real(pdf_array_get(obj, i * 2 + 0));
 		func->domain[i][1] = pdf_to_real(pdf_array_get(obj, i * 2 + 1));
@@ -1408,7 +1409,8 @@ pdf_load_function(pdf_document *xref, pdf_obj *dict)
 	{
 		func->has_range = 1;
 		func->n = pdf_array_len(obj) / 2;
-		for (i = 0; i < func->n; i++)
+		/* SumatraPDF: prevent heap overflow */
+		for (i = 0; i < MIN(func->n, MAXN); i++)
 		{
 			func->range[i][0] = pdf_to_real(pdf_array_get(obj, i * 2 + 0));
 			func->range[i][1] = pdf_to_real(pdf_array_get(obj, i * 2 + 1));
@@ -1420,7 +1422,8 @@ pdf_load_function(pdf_document *xref, pdf_obj *dict)
 		func->n = 0;
 	}
 
-	if (func->m >= MAXM || func->n >= MAXN)
+	/* SumatraPDF: allow up to FZ_MAX_COLORS colors (not one less) */
+	if (func->m > MAXM || func->n > MAXN)
 	{
 		fz_free(ctx, func);
 		fz_throw(ctx, "assert: /Domain or /Range too big");